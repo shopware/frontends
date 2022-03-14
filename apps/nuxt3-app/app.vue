fbddb97ec973594689d13f4760b5f526415439f8
patzick <13100280+patzick@users.noreply.github.com>
2022-03-14 18:36:11 +0100

    feat: nuxt3 test app

---
+++ apps/nuxt3-app/app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { getCmsPage } from "@shopware-pwa/shopware-6-client";
+
+const { apiInstance } = inject("shopware");
+const cms = ref();
+
+const { data: cmsResponse } = await useAsyncData("count", () =>
+  getCmsPage("/", {}, apiInstance)
+);
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
