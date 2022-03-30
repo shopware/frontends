876b1f68c185d1d6f63d87d9f87ee5baa7077e81
mkucmus <maciejkucmus@gmail.com>
2022-03-31 00:35:16 +0200

    feat: implement cms cross sellings

--- packages/cms-base/components/cms/block/CmsBlockCrossSelling.vue
+++ packages/cms-base/components/cms/block/CmsBlockCrossSelling.vue
@@ -1,31 +1,38 @@
-<template>
-  <div class="products-recomendations">
-    cross sells
-  </div>
-</template>
+<script setup>
+import { getTranslatedProperty } from "@shopware-pwa/helpers";
+import { useProductAssociations } from "@shopware-pwa/composables"
+const cmsPage = inject("cms-page")
+const product = computed(() => cmsPage.value?.product)
 
-<script>
-import { computed } from "@vue/composition-api"
+const {
+      loadAssociations: loadCrossSells,
+      productAssociations: crossSellCollection,
+} = useProductAssociations({
+  product: product,
+  associationContext: "cross-selling",
+})
 
-export default {
-  
-  name: "CmsElementProductCrossSelling",
-  props: {
-    content: {
-      type: Object,
-      default: () => ({}),
+onMounted(async () => {
+  loadCrossSells({
+    params: {
+      associations: {
+        seoUrls: {},
+      },
     },
-  },
-  setup(props) {
-    const crossSellCollection = computed(
-      () => props.content.data?.crossSellings || []
-    )
-
-    return {
-      crossSellCollection,
-    }
-  },
-}
+  })
+})
 </script>
 
-<style lang="scss" scoped></style>
+<template>
+  <div class="mt-10"  v-if="crossSellCollection?.length">
+    <div class="mt-4"
+            v-for="crossSellItem in crossSellCollection"
+            :key="crossSellItem.crossSelling.id"
+    >
+      <h3 class="text-sm font-medium text-gray-900">{{getTranslatedProperty(crossSellItem.crossSelling, 'name')}}</h3>
+      <div class="mt-4 flex flex-row">
+        <SwProductCard :class="`basis-1/4 mx-2`" v-for="product in crossSellItem?.products" :key="product.id" :product="product" />
+      </div>
+    </div>
+  </div>
+</template>
