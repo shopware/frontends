07056570efbffbf658a897e0a2ab190b68a13c1f
mkucmus <maciejkucmus@gmail.com>
2022-04-14 13:40:01 +0200

    chore: margin

--- packages/cms-base/components/cms/element/CmsElementProductListing.vue
+++ packages/cms-base/components/cms/element/CmsElementProductListing.vue
@@ -24,7 +24,7 @@ setInitialListing(props?.content?.data?.listing);
 <template>
   <div class="bg-white">
     <div
-      class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"
+      class="max-w-2xl mx-auto py-4 px-4 sm:py-4 sm:px-6 lg:max-w-7xl lg:px-8"
     >
       <div
         class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
