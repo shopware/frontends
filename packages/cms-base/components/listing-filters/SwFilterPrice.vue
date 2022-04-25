df09b529cc5150d04eebf10fafb36c86310ceae7
mkucmus <maciejkucmus@gmail.com>
2022-04-25 13:21:57 +0200

    fixing warnings

--- packages/cms-base/components/listing-filters/SwFilterPrice.vue
+++ packages/cms-base/components/listing-filters/SwFilterPrice.vue
@@ -30,44 +30,45 @@ watch(() => prices.max, debounce((newPrice: number, oldPrice: number) => {
 </script>
 
 <template>
-<h3 class="-mx-2 -my-3 flow-root">
-  <!-- Expand/collapse section button -->
-  <button type="button" class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500" aria-controls="filter-section-mobile-0" aria-expanded="false" @click="isFilterVisible = !isFilterVisible">
-    <span class="font-medium text-gray-900">{{ filter.label }}</span>
-    <span class="ml-6 flex items-center">
-      <!--
-        Expand icon, show/hide based on section open state.
+<div class="filter-content">
+  <h3 class="-mx-2 -my-3 flow-root">
+    <!-- Expand/collapse section button -->
+    <button type="button" class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500" aria-controls="filter-section-mobile-0" aria-expanded="false" @click="isFilterVisible = !isFilterVisible">
+      <span class="font-medium text-gray-900">{{ filter.label }}</span>
+      <span class="ml-6 flex items-center">
+        <!--
+          Expand icon, show/hide based on section open state.
 
-        Heroicon name: solid/plus-sm
-      -->
-      <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
-      </svg>
-      <!--
-        Collapse icon, show/hide based on section open state.
+          Heroicon name: solid/plus-sm
+        -->
+        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+        </svg>
+        <!--
+          Collapse icon, show/hide based on section open state.
 
-        Heroicon name: solid/minus-sm
-      -->
-      <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clip-rule="evenodd" />
-      </svg>
-    </span>
-  </button>
-</h3>
-<!-- Filter section, show/hide based on section state. -->
-<div class="pt-6" id="filter-section-mobile-0" v-show="isFilterVisible">
-  <div class="space-y-6">
-    <div class="mt-2 flex">
-      <div class="w-1/2 flex rounded-md mr-4">
-        <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> min </span>
-        <input v-model="prices.min" type="number" name="min-price" id="min-price" class="pl-2 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border border-gray-300" :placeholder="prices.min">
-      </div>
-      <div class="w-1/2 flex rounded-md ">
-        <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> max </span>
-        <input v-model="prices.max" type="number" name="max-price" id="max-price" class="pl-2 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border border-gray-300" :placeholder="prices.max">
+          Heroicon name: solid/minus-sm
+        -->
+        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clip-rule="evenodd" />
+        </svg>
+      </span>
+    </button>
+  </h3>
+  <!-- Filter section, show/hide based on section state. -->
+  <div class="pt-6" id="filter-section-mobile-0" v-show="isFilterVisible">
+    <div class="space-y-6">
+      <div class="mt-2 flex">
+        <div class="w-1/2 flex rounded-md mr-4">
+          <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> min </span>
+          <input v-model="prices.min" type="number" name="min-price" id="min-price" class="pl-2 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border border-gray-300" :placeholder="prices.min">
+        </div>
+        <div class="w-1/2 flex rounded-md ">
+          <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> max </span>
+          <input v-model="prices.max" type="number" name="max-price" id="max-price" class="pl-2 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border border-gray-300" :placeholder="prices.max">
+        </div>
       </div>
     </div>
   </div>
 </div>
-
 </template>
--- packages/cms-base/components/listing-filters/SwFilterProperties.vue
+++ packages/cms-base/components/listing-filters/SwFilterProperties.vue
@@ -6,36 +6,38 @@ const isFilterVisible = ref(false)
 </script>
 
 <template>
-<h3 class="-mx-2 -my-3 flow-root">
-  <!-- Expand/collapse section button -->
-  <button type="button" class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500" aria-controls="filter-section-mobile-0" aria-expanded="false" @click="isFilterVisible = !isFilterVisible">
-    <span class="font-medium text-gray-900">{{ filter.label }}</span>
-    <span class="ml-6 flex items-center">
-      <!--
-        Expand icon, show/hide based on section open state.
+<div class="filter-content">
+  <h3 class="-mx-2 -my-3 flow-root">
+    <!-- Expand/collapse section button -->
+    <button type="button" class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500" aria-controls="filter-section-mobile-0" aria-expanded="false" @click="isFilterVisible = !isFilterVisible">
+      <span class="font-medium text-gray-900">{{ filter.label }}</span>
+      <span class="ml-6 flex items-center">
+        <!--
+          Expand icon, show/hide based on section open state.
 
-        Heroicon name: solid/plus-sm
-      -->
-      <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
-      </svg>
-      <!--
-        Collapse icon, show/hide based on section open state.
+          Heroicon name: solid/plus-sm
+        -->
+        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+        </svg>
+        <!--
+          Collapse icon, show/hide based on section open state.
 
-        Heroicon name: solid/minus-sm
-      -->
-      <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clip-rule="evenodd" />
-      </svg>
-    </span>
-  </button>
-</h3>
-<!-- Filter section, show/hide based on section state. -->
-<div class="pt-6" id="filter-section-mobile-0" v-show="isFilterVisible">
-  <div class="space-y-6">
-    <div v-for="option in (filter.options || filter.entities)" :key="`${option.id}-${selectedOptionIds.includes(option.id)}`" class="flex items-center">
-      <input :id="`filter-mobile-${filter.id}-${option.id}`" :checked="selectedOptionIds.includes(option.id)" :name="filter.name" :value="option.name" @click="$emit('select-value', {code: filter.code, value: option.id})" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
-      <label :for="`filter-mobile-${filter.id}-${option.id}`" class="ml-3 min-w-0 flex-1 text-gray-500"> {{ option.name }} </label>
+          Heroicon name: solid/minus-sm
+        -->
+        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clip-rule="evenodd" />
+        </svg>
+      </span>
+    </button>
+  </h3>
+  <!-- Filter section, show/hide based on section state. -->
+  <div class="pt-6" id="filter-section-mobile-0" v-show="isFilterVisible">
+    <div class="space-y-6">
+      <div v-for="option in (filter.options || filter.entities)" :key="`${option.id}-${selectedOptionIds.includes(option.id)}`" class="flex items-center">
+        <input :id="`filter-mobile-${filter.id}-${option.id}`" :checked="selectedOptionIds.includes(option.id)" :name="filter.name" :value="option.name" @click="$emit('select-value', {code: filter.code, value: option.id})" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
+        <label :for="`filter-mobile-${filter.id}-${option.id}`" class="ml-3 min-w-0 flex-1 text-gray-500"> {{ option.name }} </label>
+      </div>
     </div>
   </div>
 </div>
