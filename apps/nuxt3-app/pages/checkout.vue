d01ff852043a6aaf037c466a019381eb8c5a78bb
mkucmus <maciejkucmus@gmail.com>
2022-05-23 14:53:21 +0200

    chore: description section

--- apps/nuxt3-app/pages/checkout.vue
+++ apps/nuxt3-app/pages/checkout.vue
@@ -201,6 +201,8 @@ const submitBillingAddress = async (e: Event) => {
       <div class="md:grid md:grid-cols-3 md:gap-6 mt-10">
         <div class="md:col-span-1">
           <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">Order summary</h3>
+            <p class="mt-1 text-sm text-gray-600">Order details and totals.</p>
           </div>
         </div>
         <div class="mt-5 md:mt-0 md:col-span-2">
