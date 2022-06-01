8b91e402df2397ff45ee8e17dfed386a0b19f03b
mkucmus <maciejkucmus@gmail.com>
2022-06-01 14:21:56 +0200

    feat(cms-base): block image gallery, block text teaser section

---
+++ packages/cms-base/components/cms/block/CmsBlockImageGallery.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import SwProductGallery from "../../SwProductGallery.vue";
+
+const $props = defineProps({
+  content: Object,
+});
+const product = computed(() => ({
+    // create a fake product to have only the media gallery
+    media: $props.content?.slots?.[0]?.data?.sliderItems || []
+}));
+</script>
+<template>
+    <div class="container mx-auto">
+        <SwProductGallery :product="product" />
+    </div>
+</template>
