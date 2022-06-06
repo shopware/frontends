aa4bcf3d33a0381dca0729cfef61379975c6f9a0
mkucmus <maciejkucmus@gmail.com>
2022-06-06 15:51:40 +0200

    feat(cms-base): block image text

--- packages/cms-base/components/cms/block/CmsBlockImageText.vue
+++ packages/cms-base/components/cms/block/CmsBlockImageText.vue
@@ -1,80 +1,19 @@
-<template>
-  <article class="cms-block-image-text">
-    <CmsGenericElement
-      :content="getLeftContent"
-      class="cms-block-image-text__image"
-    />
-    <CmsGenericElement
-      :content="getRightContent"
-      class="cms-block-image-text__text"
-    />
-  </article>
-</template>
-
-<script>
-// import CmsGenericElement from "sw-cms/CmsGenericElement";
-
-export default {
-  name: "CmsBlockImageText",
-
-  components: {
-    // CmsGenericElement,
-  },
-
-  props: {
-    content: {
-      type: Object,
-      default: () => ({}),
-    },
-  },
-
-  computed: {
-    getSlots() {
-      return this.content.slots || [];
-    },
-
-    getLeftContent() {
-      return this.getSlots.find(({ slot }) => slot === "left");
-    },
-    getRightContent() {
-      return this.getSlots.find(({ slot }) => slot === "right");
-    },
-  },
-};
-// <style lang="scss" scoped>
-// @import "@/assets/scss/variables";
-
-// .cms-block-image-text {
-//   display: flex;
-//   flex-direction: column;
-//   margin-right: 0;
-//   margin-left: 0;
-
-//   &__image,
-//   &__text {
-//     margin: var(--spacer-sm);
-//     flex: 1;
-//     --image-height: 340px;
-//     --image-width: 100%;
-//   }
-
-//   @include for-desktop {
-//     align-items: center;
-//     flex-direction: row;
-//     justify-content: space-around;
-
-//     .cms-block-image-text__image {
-//       margin-bottom: 0;
-//       margin-right: var(--spacer-sm);
-//       margin-top: 0;
-//     }
-
-//     .cms-block-image-text__text {
-//       margin-bottom: 0;
-//       margin-left: var(--spacer-sm);
-//       margin-top: 0;
-//     }
-//   }
-// }
-// </style>
+<script setup>
+const $props = defineProps(["content"]);
+const getSlots = computed(() => $props.content?.slots || []);
+const getLeftContent = computed(() => getSlots.value.find((slot) => slot.slot === "left"));
+const getRightContent = computed(() => getSlots.value.find((slot) => slot.slot === "right"));
 </script>
+<template>
+  <div class="cms-block-image-text flex flex-col md:flex-row container mx-auto mb-8 mt-8">
+    <div class="md:basis-1/2 self-center">
+      <CmsGenericElement
+        :content="getLeftContent"
+      />
+    </div>
+    <div class="md:basis-1/2 md:pl-8">
+      <CmsGenericElement
+      :content="getRightContent" />
+    </div>
+  </div>
+</template>
