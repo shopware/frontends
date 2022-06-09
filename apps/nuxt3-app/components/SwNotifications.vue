362f19579f7d91cbaff946c88b6bbee28234564f
mkucmus <maciejkucmus@gmail.com>
2022-06-09 22:40:56 +0200

    feat: notification functionality

--- apps/nuxt3-app/components/SwNotifications.vue
+++ apps/nuxt3-app/components/SwNotifications.vue
@@ -1,22 +1,9 @@
-<script setup>
+<script setup lang="ts">
 import { useNotifications } from "@shopware-pwa/composables"
-
 const { notifications, removeOne } = useNotifications()
 </script>
 <template>
-<div class="mx-auto fixed top-0 w-full z-50 drop-shadow-md">
-  <div class="flex p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800" role="alert"
-    :key="notification.id"
-    v-for="notification in notifications"
-  >
-    <svg class="inline flex-shrink-0 mr-3 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-    <div>
-      {{ notification.message }}
-    </div>
-    <button type="button" @click="removeOne(notification.id)" class="ml-auto -mx-1.5 -my-1.5 bg-blue-100 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex h-8 w-8 dark:bg-blue-200 dark:text-blue-600 dark:hover:bg-blue-300" data-dismiss-target="#alert-1" aria-label="Close">
-      <span class="sr-only">Close</span>
-      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-    </button>
-  </div>
+<div class="fixed right-5 top-5 z-50">
+  <SwNotification v-for="notification in notifications" :notification="notification" :key="notification.id" @click:close="removeOne(notification.id)"/>
 </div>
 </template>
