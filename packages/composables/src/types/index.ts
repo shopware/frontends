d5fcd2edbfba216917d1d69e406ab3c78dc8b936
mkucmus <maciejkucmus@gmail.com>
2022-06-28 22:32:46 +0200

    refactor: remove obsolete types

--- packages/composables/src/types/index.ts
+++ packages/composables/src/types/index.ts
@@ -1,63 +1,31 @@
 import { ProductResponse as LegacyProductResponse } from "@shopware-pwa/commons/interfaces/response/ProductResult";
-import { EntityResult } from "@shopware-pwa/commons";
-import { Product as LegacyProduct } from "@shopware-pwa/commons/interfaces/models/content/product/Product";
-import { Category as LegacyCategory } from "@shopware-pwa/commons/interfaces/models/content/category/Category";
+import { LandingPage } from "@shopware-pwa/commons/interfaces/models/content/landing-page/LandingPage";
+import { Category } from "@shopware-pwa/commons/interfaces/models/content/category/Category";
 import { CmsPage } from "@shopware-pwa/commons/interfaces/models/content/cms/CmsPage";
-import { SeoUrl as LegacySeoUrl } from "@shopware-pwa/commons/interfaces/models/content/navigation/Navigation";
 export { SearchFilterType } from "@shopware-pwa/commons";
 
-export type SeoUrl = LegacySeoUrl;
-type Category = LegacyCategory;
-type Product = LegacyProduct & { cmsPage: null | CmsPage };
-
-export type SeoResult = EntityResult<"seo_url", LegacySeoUrl[]>;
 /**
  * API responses' types
  * A combination of legacy response types and new response types
  * @TODO: to be unified in the final types package
  */
-export type ProductResponse = LegacyProductResponse & {
-  product: Product;
-};
-
 export type CategoryResponse = Category;
-
-export type LandingPageResponse = {
-  active: boolean;
-  translations: null | unknown;
-  cmsPage: null | CmsPage;
-  cmsPageId: string;
-  name: string;
-  metaTitle: null | string;
-  metaDescription: null | string;
-  keywords: null | string;
-  url: string;
-  slotConfig: null | unknown[];
-  seoUrls: null | SeoUrl[];
-  _uniqueIdentifier: string;
-  versionId: string;
-  translated: {
-    [key: string]: string;
-  };
-  createdAt: string;
-  updatedAt: null | string;
-  extensions: unknown;
-  id: string;
-  customFields: null | unknown;
-  cmsPageVersionId: string;
-  apiAlias: "landing_page";
-};
-
+export type ProductResponse = LegacyProductResponse;
+export type LandingPageResponse = LandingPage;
 /**
  * Types for internal usage.
  *
  * @TODO: rethink the format of the types
  */
 
+export const PRODUCT_ROUTE_NAME = "frontend.detail.page";
+export const CATEGORY_ROUTE_NAME = "frontend.navigation.page";
+export const LANDING_PAGE_ROUTE_NAME = "frontend.landing.page";
+
 export type ResourceType =
-  | "frontend.navigation.page"
-  | "frontend.detail.page"
-  | "frontend.landing.page";
+  | typeof PRODUCT_ROUTE_NAME
+  | typeof CATEGORY_ROUTE_NAME
+  | typeof LANDING_PAGE_ROUTE_NAME;
 
 type CategoryCmsResult = {
   category: CategoryResponse;
@@ -66,7 +34,7 @@ type CategoryCmsResult = {
 type ProductCmsResult = ProductResponse;
 
 type LandingPageCmsResult = {
-  landingPage: LandingPageResponse;
+  landingPage: LandingPage;
 };
 
 export type SearchCmsResult = (
