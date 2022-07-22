b6e32af1f187e3e2d0ccbe3559cf4af47dd068af
rittou <rittou.xiii@gmail.com>
2022-07-22 10:14:44 +0700

    feat(cms-base): Implement CmsBlockImageCover

--- packages/cms-base/components/cms/block/CmsBlockImageCover.vue
+++ packages/cms-base/components/cms/block/CmsBlockImageCover.vue
@@ -10,5 +10,13 @@ const { getSlotContent } = useCmsBlock(props.content);
 const cmsContent = getSlotContent("image");
 </script>
 <template>
-  <CmsGenericElement v-if="cmsContent" :content="cmsContent" />
+  <div class="cms-block-image-cover">
+    <CmsGenericElement v-if="cmsContent" :content="cmsContent" />
+  </div>
 </template>
+
+<style scoped>
+.cms-block-image-cover .cms-element-image {
+  @apply aspect-square object-cover;
+}
+</style>
