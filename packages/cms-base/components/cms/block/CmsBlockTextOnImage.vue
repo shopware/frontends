83060618e4c273aa42adfa3afd06f55dd04e8f87
rittou <rittou.xiii@gmail.com>
2022-07-26 09:59:45 +0700

    feat(cms-base): Implement CmsBlockTextOnImage

--- packages/cms-base/components/cms/block/CmsBlockTextOnImage.vue
+++ packages/cms-base/components/cms/block/CmsBlockTextOnImage.vue
@@ -8,18 +8,12 @@ const props = defineProps<{
 const { getSlotContent } = useCmsBlock(props.content);
 
 const slotContent = getSlotContent("content");
-
-const getBackgroundStyle = computed(() => {
-  return `background-image: url('${props.content.backgroundMedia?.url}');`;
-});
 </script>
 
 <template>
-  <article class="min-h-sm bg-center bg-cover p-10" :style="getBackgroundStyle">
-    <CmsGenericElement
-      v-if="slotContent"
-      :content="slotContent"
-      class="cms-block-text-on-image__content"
-    />
-  </article>
+  <CmsGenericElement
+    v-if="slotContent"
+    :content="slotContent"
+    class="cms-block-text-on-image min-h-md"
+  />
 </template>
