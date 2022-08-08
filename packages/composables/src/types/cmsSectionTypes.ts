95909a113080fe795273783c653983ed6ff1863a
patzick <13100280+patzick@users.noreply.github.com>
2022-08-08 12:48:55 +0200

    chore: common section block typing

--- packages/composables/src/types/cmsSectionTypes.ts
+++ packages/composables/src/types/cmsSectionTypes.ts
@@ -1,13 +1,15 @@
-import { CmsBlock, CmsSection } from "@shopware-pwa/types";
+import { CmsBlock, CmsSection, SectionType } from "@shopware-pwa/types";
+
+export type CmsSectionBlock<
+  TYPE extends SectionType,
+  SECTION_POSITIONS
+> = CmsSection & {
+  type: TYPE;
+  blocks: Array<CmsBlock & { sectionPosition: SECTION_POSITIONS }>;
+};
 
 // Section Default can have only one container for blocks: main (full page)
-export type CmsSectionDefault = CmsSection & {
-  type: "default";
-  blocks: Array<CmsBlock & { sectionPosition: "main" }>;
-};
+export type CmsSectionDefault = CmsSectionBlock<"default", "main">;
 
 // Section Sidebar can have blocks in two different containers: sidebar (left), and main (right)
-export type CmsSectionSidebar = CmsSection & {
-  type: "sidebar";
-  blocks: Array<CmsBlock & { sectionPosition: "sidebar" | "main" }>;
-};
+export type CmsSectionSidebar = CmsSectionBlock<"sidebar", "sidebar" | "main">;
