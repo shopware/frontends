2c5acdb62078feb1cdb61e3b2bad5c00a339f111
Thu Vo <thu.vo@shopware.com>
2022-08-11 16:03:08 +0000

    feat(nuxt3-app): Implement Password Recovery Page (SWF-38)

---
+++ packages/composables/src/useCustomerPassword.ts
@@ -0,0 +1,81 @@
+import { UnwrapRef, reactive } from "vue-demi";
+import {
+  updatePassword as apiUpdatePassword,
+  resetPassword as apiResetPassword,
+  CustomerUpdatePasswordParam,
+  CustomerResetPasswordParam,
+} from "@shopware-pwa/shopware-6-client";
+import { ClientApiError, ShopwareError } from "@shopware-pwa/types";
+import { useShopwareContext } from "./useShopwareContext";
+
+/**
+ * interface for {@link useCustomerPassword} composable
+ *
+ * @beta
+ */
+export type UseCustomerPassword = {
+  errors: UnwrapRef<{
+    resetPassword: ShopwareError[];
+    updatePassword: ShopwareError[];
+  }>;
+  updatePassword: (
+    updatePasswordData: CustomerUpdatePasswordParam
+  ) => Promise<boolean>;
+  resetPassword: (
+    resetPasswordData: CustomerResetPasswordParam
+  ) => Promise<boolean>;
+};
+
+/**
+ * Composable for customer password management. Options - {@link UseCustomerPassword}
+ *
+ * @beta
+ */
+export function useCustomerPassword(): UseCustomerPassword {
+  const { apiInstance } = useShopwareContext();
+
+  const errors: UnwrapRef<{
+    resetPassword: ShopwareError[];
+    updatePassword: ShopwareError[];
+  }> = reactive({
+    resetPassword: [],
+    updatePassword: [],
+  });
+
+  /**
+   * Change customer's current password
+   */
+  const updatePassword = async (
+    updatePasswordData: CustomerUpdatePasswordParam
+  ): Promise<boolean> => {
+    try {
+      errors.updatePassword = [];
+      await apiUpdatePassword(updatePasswordData, apiInstance);
+    } catch (e) {
+      errors.updatePassword = (e as ClientApiError).messages;
+      return false;
+    }
+    return true;
+  };
+
+  /**
+   * Reset customer's password
+   */
+  const resetPassword = async (
+    resetPasswordData: CustomerResetPasswordParam
+  ): Promise<boolean> => {
+    try {
+      await apiResetPassword(resetPasswordData, apiInstance);
+    } catch (e) {
+      errors.resetPassword = (e as ClientApiError).messages;
+      return false;
+    }
+    return true;
+  };
+
+  return {
+    updatePassword,
+    resetPassword,
+    errors,
+  };
+}
