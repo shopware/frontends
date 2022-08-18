46708c9b6f6b461806d2cf1b87e217c245b09765
Quan Do <q.do@shopware.com>
2022-08-18 08:48:17 +0000

    feat(cms-base): implement CmsElementCrossSelling

--- packages/cms-base/components/SwProductCard.vue
+++ packages/cms-base/components/SwProductCard.vue
@@ -7,6 +7,7 @@ import {
   getProductThumbnailUrl,
   getProductUrl,
   getProductVariantsFromPrice,
+  getTranslatedProperty,
   getProductFromPrice,
 } from "@shopware-pwa/helpers-next";
 import { Product } from "@shopware-pwa/types";
@@ -15,6 +16,8 @@ import SwStarIcon from "./SwStarIcon.vue";
 import SwHeartIcon from "./SwHeartIcon.vue";
 const heartIconType = "svg";
 const heartIconClass = "wishlist-heart-icon";
+
+const { currency } = useSessionContext();
 
 const { pushSuccess, pushInfo } = useNotifications();
 
@@ -95,23 +98,10 @@ const fromPrice = getProductFromPrice(props.product);
 const ratingAverage: Ref<number> = computed(() =>
   props.product.ratingAverage ? Math.round(props.product.ratingAverage) : 0
 );
-const isProductListing = computed(() => props.isProductListing);
-const purchaseUnit = computed(() => props.product.purchaseUnit);
-const unitName = computed(() => props.product.unit?.name);
-const productPrice = computed(() =>
-  !fromPrice ? getPrice(props.product) : fromPrice
-);
 </script>
 
 <template>
   <div class="sw-product-card group relative flex flex-col justify-between">
-    <button
-      type="button"
-      @click="addToWishlistFn"
-      class="absolute top-2 right-2 z-40"
-    >
-      <SwHeartIcon :is-empty="isInWishlist ? true : false" />
-    </button>
     <div
       :class="[
         'w-full rounded-md overflow-hidden hover:opacity-75',
@@ -132,64 +122,49 @@ const productPrice = computed(() =>
         }"
       />
     </div>
-    <div class="mt-4 justify-between h-45">
-      <div class="h-40">
-        <div class="min-h-14">
-          <h3 class="text-lg font-semibold text-gray-700">
-            <router-link class="line-clamp-2 h-12" :to="getProductUrl(product)">
-              <span
-                aria-hidden="true"
-                class="absolute inset-0 bottom-40px"
-              ></span>
-              {{ getProductName({ product }) }}
-            </router-link>
-          </h3>
-        </div>
-        <button
-          type="button"
-          @click="addToWishlistFn"
-          class="absolute top-2 right-2"
-        >
-          <SwHeartIcon :is-empty="isInWishlist ? true : false" />
-        </button>
-        <div
-          class="line-clamp-4 mt-2 text-sm text-gray-500 max-h-20"
-          v-html="product.description?.substring(0, 100) + '...'"
-        ></div>
-        <div v-if="purchaseUnit && unitName" class="mt-1 text-sm h-5">
-          <span class="font-semibold"> Content: </span>
-          <span class="font-light"> {{ purchaseUnit }} {{ unitName }} </span>
-        </div>
+    <button
+      type="button"
+      @click="addToWishlistFn"
+      class="absolute top-2 right-2"
+    >
+      <SwHeartIcon :is-empty="!!isInWishlist" />
+    </button>
+    <div class="mt-4 flex flex-col justify-between flex-1">
+      <div>
+        <h3 class="text-base font-bold text-gray-700">
+          <router-link class="line-clamp-2 h-12" :to="getProductUrl(product)">
+            {{ getProductName({ product }) }}
+          </router-link>
+        </h3>
         <p
-          v-if="!isProductListing"
-          class="mt-2 text-sm text-gray-500 min-h-30px h-10"
-        >
+          v-if="layoutType === 'standard'"
+          class="line-clamp-4 mt-2 text-sm text-gray-500 h-20 overflow-hidden"
+          v-html="getTranslatedProperty(product, 'description')"
+        />
+        <div class="mt-2 flex gap-2 flex-wrap">
           <span
             v-for="option in product?.options"
             :key="option.id"
-            class="bg-gray-400 mb-2 mr-2 text-white rounded p-0.5"
+            class="bg-gray-400 text-sm text-white rounded py-1 px-2"
           >
             {{ option.group.name }}: {{ option.name }}
           </span>
-        </p>
+        </div>
       </div>
-      <div class="flex flex-row mt-3 justify-between h-5">
+      <div class="flex flex-row mt-3 justify-between">
         <div class="text-sm font-medium text-gray-900">
-          <SwPrice v-if="productPrice" :value="productPrice">
-            <template v-if="fromPrice" #beforePrice> From </template>
-          </SwPrice>
+          {{ !fromPrice ? getPrice(product) : `From ${fromPrice}` }}
+          {{ currency?.symbol }}
         </div>
         <div v-if="!isProductListing" class="sw-product-rating inline-flex">
-          <div v-for="value in ratingAverage">
-            <SwStarIcon />
-          </div>
+          <div v-for="value in ratingAverage"><SwStarIcon /></div>
           <div v-for="value in 5 - ratingAverage">
             <SwStarIcon :is-empty="true" />
           </div>
         </div>
       </div>
     </div>
-    <div class="mt-2">
+    <div class="mt-3">
       <button
         v-if="!fromPrice"
         type="button"
--- packages/cms-base/components/cms/block/CmsBlockCrossSelling.vue
+++ packages/cms-base/components/cms/block/CmsBlockCrossSelling.vue
@@ -1,69 +1,14 @@
 <script setup lang="ts">
-import {
-  CmsBlockCrossSelling,
-  CmsElementCrossSelling,
-} from "@shopware-pwa/composables-next";
-import { getTranslatedProperty } from "@shopware-pwa/helpers-next";
-import { CmsProductPageResponse } from "@shopware-pwa/types";
+import { CmsBlockCrossSelling } from "@shopware-pwa/composables-next";
 
 const props = defineProps<{
   content: CmsBlockCrossSelling;
 }>();
-
-const { cmsContent } = useCms();
-const product = computed(
-  () => (cmsContent.value as CmsProductPageResponse)?.product
-);
-
 const { getSlotContent } = useCmsBlock(props.content);
-const slotContent = getSlotContent("content") as CmsElementCrossSelling;
-
-const crossSellCollection = ref(slotContent.data?.crossSellings || []);
-const {
-  loadAssociations: loadCrossSells,
-  productAssociations: crossSellAssociations,
-} = useProductAssociations({
-  product: product,
-  associationContext: "cross-selling",
-});
-onMounted(async () => {
-  if (!product.value) {
-    return;
-  }
-  loadCrossSells({
-    params: {
-      associations: {
-        seoUrls: {},
-      },
-    },
-  } as any);
-});
-
-watch(crossSellAssociations, (newCollection) => {
-  if (newCollection.length) {
-    crossSellCollection.value = newCollection;
-  }
-});
+const content = getSlotContent("content");
 </script>
-
 <template>
-  <div class="container mx-auto mt-8 mb-8" v-if="crossSellCollection?.length">
-    <div
-      class="mt-4"
-      v-for="crossSellItem in crossSellCollection"
-      :key="crossSellItem.id"
-    >
-      <h3 class="text-sm font-medium text-gray-900">
-        {{ getTranslatedProperty(crossSellItem, "name") }}
-      </h3>
-      <div class="mt-4 flex flex-row">
-        <SwProductCard
-          :class="`basis-1/4 mx-2`"
-          v-for="product in crossSellItem?.assignedProducts"
-          :key="product.id"
-          :product="product"
-        />
-      </div>
-    </div>
+  <div class="cms-block-cross-selling">
+    <CmsGenericElement :content="content" />
   </div>
 </template>
