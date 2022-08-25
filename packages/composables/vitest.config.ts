48eabcaedc2f894426404d5dad2273e51ec772b7
Patryk Tomczyk <p.tomczyk@shopware.com>
2022-08-25 17:04:38 +0000

    chore: bump dependencies

--- packages/composables/vitest.config.ts
+++ packages/composables/vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "happy-dom",
+    coverage: {
+      enabled: true,
+      // "100": true, // TODO: our goal ;)
+    },
   },
 });
--- packages/helpers/vitest.config.ts
+++ packages/helpers/vitest.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({});
+export default defineConfig({
+  test: {
+    coverage: {
+      enabled: true,
+      // "100": true, // TODO: our goal ;)
+    },
+  },
+});
