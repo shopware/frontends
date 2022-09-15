aa99fb7e906ca6dadc25cbe44b87d624ff58fee7
Maciej Daniłowicz <m.danilowicz@shopware.com>
2022-09-15 08:42:18 +0200

    feat(doc): lh best practices (SWF-82)

--- apps/docs/src/best-practices/performance.md
+++ apps/docs/src/best-practices/performance.md
@@ -1,1 +1,31 @@
 # Performance
+
+## Lighthouse performance checklist
+
+:::warning
+Please remember that the Lighthouse score should be checked only on the production build.
+:::
+
+### Performance
+
+- [ ] images have appropriate resolution
+- [ ] images are in `WebP` format
+- [ ] third part code is loaded asynchronously
+- [ ] images are lazy loaded
+- [ ] all custom event listeners are destroyed with their components
+
+### Accessibility
+
+- [ ] all images are described by `alt` attribute
+- [ ] contrast is correct
+- [ ] `aria-label` are added to HTML tags
+
+### Best Practices
+
+- [ ] `https` connection is used
+- [ ] semantic html structure
+
+### SEO
+
+- [ ] `robots.txt` is added
+- [ ] all pages have metadata (title, description, tags)
--- templates/vue-demo-store/components/layout/LayoutSideMenu.vue
+++ templates/vue-demo-store/components/layout/LayoutSideMenu.vue
@@ -37,103 +37,105 @@ const triggerCollapse = (id: string) => {
   <button class="md:hidden" @click="isSideMenuOpened = true" aria-label="menu">
     <div i-carbon-menu text-xl />
   </button>
-  <div
-    v-show="isSideMenuOpened"
-    class="relative z-40 md:hidden"
-    role="dialog"
-    aria-modal="true"
-  >
-    <div class="fixed inset-0 bg-black opacity-25"></div>
-    <div class="fixed inset-0 z-40 flex" @click="isSideMenuOpened = false">
-      <div
-        class="relative flex flex-col w-full max-w-xs overflow-y-auto bg-white shadow-xl"
-      >
-        <div class="flex px-4 py-5">
-          <button
-            type="button"
-            class="inline-flex items-center justify-center p-2 -m-2 text-gray-400 rounded-md"
-            @click="isSideMenuOpened = false"
-          >
-            <span class="sr-only">Close menu</span>
-            <div i-carbon-close text-3xl />
-          </button>
-        </div>
-
-        <div class="max-w-2xl">
-          <aside aria-label="Sidebar">
-            <div class="overflow-y-auto">
-              <ul class="flex flex-col p-0 space-y-2">
-                <li
-                  v-for="(navigationElement, index) in navigationElements"
-                  :key="navigationElement.id"
-                >
-                  <router-link
-                    :to="getCategoryUrl(navigationElement)"
-                    @click="isSideMenuOpened = false"
-                    class="flex items-center w-full px-5 py-3 text-base font-normal text-gray-900 break-all hover:bg-gray-100"
+  <client-only>
+    <div
+      v-if="isSideMenuOpened"
+      class="relative z-40 md:hidden"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div class="fixed inset-0 bg-black opacity-25"></div>
+      <div class="fixed inset-0 z-40 flex" @click="isSideMenuOpened = false">
+        <div
+          class="relative flex flex-col w-full max-w-xs overflow-y-auto bg-white shadow-xl"
+        >
+          <div class="flex px-4 py-5">
+            <button
+              type="button"
+              class="inline-flex items-center justify-center p-2 -m-2 text-gray-400 rounded-md"
+              @click="isSideMenuOpened = false"
+            >
+              <span class="sr-only">Close menu</span>
+              <div i-carbon-close text-3xl />
+            </button>
+          </div>
+          <!-- <client-only></client-only> -->
+          <div class="max-w-2xl">
+            <aside aria-label="Sidebar">
+              <div class="overflow-y-auto">
+                <ul class="flex flex-col p-0 space-y-2">
+                  <li
+                    v-for="(navigationElement, index) in navigationElements"
+                    :key="navigationElement.id"
                   >
-                    <span class="flex-1">
-                      {{ navigationElement.name }}
-                    </span>
-                    <button
-                      class="flex items-center w-12 p-4 -m-4 h-11"
-                      v-if="navigationElement?.children?.length"
-                      @click.stop.prevent="
-                        triggerCollapse(navigationElement.id)
-                      "
+                    <router-link
+                      :to="getCategoryUrl(navigationElement)"
+                      @click="isSideMenuOpened = false"
+                      class="flex items-center w-full px-5 py-3 text-base font-normal text-gray-900 break-all hover:bg-gray-100"
                     >
-                      <div
-                        v-if="!collapseItems[index]?.isCollapsed"
-                        i-carbon-chevron-up
-                        text-xl
-                      />
-                      <div v-else i-carbon-chevron-down text-xl />
-                    </button>
-                  </router-link>
-
-                  <div
-                    v-if="
-                      navigationElement.media &&
-                      !collapseItems[index]?.isCollapsed
-                    "
-                    class="relative"
-                  >
-                    <div class="overflow-hidden">
-                      <img
-                        :src="getCategoryImageUrl(navigationElement)"
-                        class="object-cover object-center"
-                        alt="Category image"
-                      />
-                    </div>
-                  </div>
-                  <ul
-                    v-if="
-                      navigationElement?.children?.length &&
-                      !collapseItems[index]?.isCollapsed
-                    "
-                    class="px-0 py-2 m-0"
-                  >
-                    <li
-                      v-for="childElement in navigationElement.children"
-                      :key="childElement.id"
-                    >
-                      <router-link
-                        :to="getCategoryUrl(childElement)"
-                        @click="isSideMenuOpened = false"
-                        class="flex items-center p-3 text-base font-normal text-gray-500 break-all hover:bg-gray-100 pl-11"
+                      <span class="flex-1">
+                        {{ navigationElement.name }}
+                      </span>
+                      <button
+                        class="flex items-center w-12 p-4 -m-4 h-11"
+                        v-if="navigationElement?.children?.length"
+                        @click.stop.prevent="
+                          triggerCollapse(navigationElement.id)
+                        "
                       >
-                        <span>
-                          {{ childElement.name }}
-                        </span>
-                      </router-link>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </aside>
+                        <div
+                          v-if="!collapseItems[index]?.isCollapsed"
+                          i-carbon-chevron-up
+                          text-xl
+                        />
+                        <div v-else i-carbon-chevron-down text-xl />
+                      </button>
+                    </router-link>
+
+                    <div
+                      v-if="
+                        navigationElement.media &&
+                        !collapseItems[index]?.isCollapsed
+                      "
+                      class="relative"
+                    >
+                      <div class="overflow-hidden">
+                        <img
+                          :src="getCategoryImageUrl(navigationElement)"
+                          class="object-cover object-center"
+                          alt="Category image"
+                        />
+                      </div>
+                    </div>
+                    <ul
+                      v-if="
+                        navigationElement?.children?.length &&
+                        !collapseItems[index]?.isCollapsed
+                      "
+                      class="px-0 py-2 m-0"
+                    >
+                      <li
+                        v-for="childElement in navigationElement.children"
+                        :key="childElement.id"
+                      >
+                        <router-link
+                          :to="getCategoryUrl(childElement)"
+                          @click="isSideMenuOpened = false"
+                          class="flex items-center p-3 text-base font-normal text-gray-500 break-all hover:bg-gray-100 pl-11"
+                        >
+                          <span>
+                            {{ childElement.name }}
+                          </span>
+                        </router-link>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </aside>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </client-only>
 </template>
