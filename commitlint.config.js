104974d7aae073359424b8e5284b6dbdd82d277b
Maciej Danilowicz <m.danilowicz@shopware.com>
2022-09-19 10:43:56 +0000

    fix: commit lint references (NTR) - test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,26 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "references-empty": [2, "never"],
+    "references-empty-ntr": [2, "never"],
   },
   parserPreset: {
     parserOpts: {
-      issuePrefixes: ["SWF-"],
+      issuePrefixes: ["SWF-", "NTR"],
     },
   },
+  plugins: [
+    {
+      rules: {
+        "references-empty-ntr": (parsed) => {
+          let notEmpty = parsed.references.length > 0;
+          if (!notEmpty && parsed.subject.includes("(NTR)")) notEmpty = true;
+
+          return [
+            notEmpty,
+            "issue references may not be empty. Use '(NTR)' if there is no issue for this MR",
+          ];
+        },
+      },
+    },
+  ],
 };
