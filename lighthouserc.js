e2167e3c274badf8c9a89ac993c04759337706ff
Krzysztof Stala <k.stala@shopware.com>
2022-09-22 08:13:16 +0000

    feat: add lighthouse report to CI process

---
+++ lighthouserc.js
@@ -0,0 +1,44 @@
+
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 5,
+      url: ['https://frontends-demo.vercel.app/'],
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ],
+        skipAudits: ['uses-http2'],
+        chromeFlags: '--no-sandbox',
+      },
+    },
+    assert: {
+      assertions: {
+        'categories:performance': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'median-run' },
+        ],
+        'categories:accessibility': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'median-run' },
+        ],
+        'categories:best-practices': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'median-run' },
+        ],
+        'categories:seo': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'median-run' },
+        ],
+      },
+    },
+    upload: {
+      target: 'lhci',
+      serverBaseUrl: 'https://lhci-server.apps.shopware.io',
+      token: '6192d5d3-055a-4e82-be64-41cc7434c3f9'
+    },
+  },
+}
