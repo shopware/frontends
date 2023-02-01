7aa2c640203e24b6abb5b8fe0eff6d7b72e6b08d
Maciej Danilowicz <m.danilowicz@shopware.com>
2023-02-01 06:56:37 +0000

    feat(cms): product listing filters (SWF-229)

---
+++ .changeset/serious-cheetahs-tan.md
@@ -0,0 +1,5 @@
+---
+"@shopware-pwa/cms-base": minor
+---
+
+Adjust products listing filters
--- packages/cms-base/components/listing-filters/SwFilterProperties.vue
+++ packages/cms-base/components/listing-filters/SwFilterProperties.vue
@@ -15,23 +15,19 @@ const isFilterVisible = ref<boolean>(false);
 const toggle = () => {
   isFilterVisible.value = !isFilterVisible.value;
 };
-
-const dropdownElement = ref(null);
-onClickOutside(dropdownElement, () => (isFilterVisible.value = false));
 </script>
 
 <template>
-  <div ref="dropdownElement" class="filter-content">
-    <h3 class="-mx-2 -my-3 flow-root">
+  <div class="border-b border-gray-200 py-6">
+    <h3 class="-my-3 flow-root">
       <button
         type="button"
-        class="border-1 border-gray-500 px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500 rounded"
+        class="flex w-full items-center justify-between bg-white py-2 text-base text-gray-400 hover:text-gray-500"
         @click="toggle"
       >
         <span class="font-medium text-gray-900">{{ filter.label }}</span>
         <span class="ml-6 flex items-center">
-          <div
-            class="h-5 w-5"
+          <i
             :class="[
               !isFilterVisible
                 ? 'i-carbon-chevron-down'
@@ -41,29 +37,29 @@ onClickOutside(dropdownElement, () => (isFilterVisible.value = false));
         </span>
       </button>
     </h3>
-    <div
-      :class="[
-        `absolute bg-white border-2 border-gray-300 max-h-70 overflow-auto p-3 rounded z-1000`,
-        { hidden: !isFilterVisible },
-      ]"
-    >
-      <div class="space-y-6">
-        <div
-          v-for="option in filter.options || filter.entities"
-          :key="`${option.id}-${selectedOptionIds?.includes(option.id)}`"
-          class="flex items-center"
-        >
-          <label
-            :for="`filter-mobile-${filter.code}-${option.id}`"
-            class="min-w-0 flex-1 text-gray-500 flex items-center gap-2 relative"
+    <transition name="fade" mode="out-in">
+      <div v-show="isFilterVisible" class="pt-6" id="filter-section-0">
+        <div class="space-y-4">
+          <div
+            v-for="option in filter.options || filter.entities"
+            :key="`${option.id}-${selectedOptionIds?.includes(option.id)}`"
             @click="
               emits('select-value', { code: filter.code, value: option.id })
             "
+            class="flex items-center"
           >
+            <input
+              :id="`filter-mobile-${filter.code}-${option.id}`"
+              :checked="selectedOptionIds?.includes(option.id)"
+              :name="filter.name"
+              :value="option.name"
+              type="checkbox"
+              class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+            />
+
             <div v-if="option.media?.url">
-              <!-- "Active status" will be changed with the redesign task  -->
               <img
-                class="h-6 w-6"
+                class="ml-2 h-4 w-4"
                 :src="option.media.url"
                 :alt="option.media.translated?.alt"
                 :class="{
@@ -73,30 +69,31 @@ onClickOutside(dropdownElement, () => (isFilterVisible.value = false));
                 }"
               />
             </div>
-            <!-- "Active status" will be changed with the redesign task  -->
             <div
               v-else-if="option.colorHexCode"
-              class="h-6 w-6"
+              class="ml-2 h-4 w-4"
               :style="`background-color: ${option.colorHexCode}`"
               :class="{
                 'border-blue border-2': selectedOptionIds?.includes(option.id),
               }"
             />
-            <input
-              v-else
-              :id="`filter-mobile-${filter.code}-${option.id}`"
-              :checked="selectedOptionIds?.includes(option.id)"
-              :name="filter.name"
-              :value="option.name"
-              type="checkbox"
-              class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-            />
-            <div>
+            <label for="filter-color-0" class="ml-3 text-sm text-gray-600">
               {{ getTranslatedProperty(option, "name") }}
-            </div>
-          </label>
+            </label>
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
