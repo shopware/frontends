45b211884cdbad86f272695c50775c0deaf48f55
Björn Meyer <b.meyer@shopware.com>
2023-02-17 11:24:17 +0100

    feat: improve top navigation (SWF-263)

--- templates/vue-demo-store/components/layout/LayoutTopNavigation.vue
+++ templates/vue-demo-store/components/layout/LayoutTopNavigation.vue
@@ -41,45 +41,70 @@ onClickOutside(menuHtmlElement, () => (currentMenuPosition.value = null));
         <div
           v-if="
             currentMenuPosition === navigationElement.id &&
-            navigationElement?.children?.length
+              navigationElement?.children?.length
           "
-          class="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
+          class="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md xl:max-w-screen-sm sm:px-0 lg:ml-0 lg:left-1/4 lg:-translate-x-1/4"
           @mouseleave="currentMenuPosition = null"
         >
           <div
             class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden"
           >
-            <div
-              v-for="childElement in navigationElement.children"
+            <template
+              v-for="(childElement, index) in navigationElement.children"
               :key="childElement.id"
-              class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-6 sm:pb-0"
             >
-              <RouterLink
-                :to="'/' + childElement?.seoUrls?.[0]?.seoPathInfo"
-                class="flex justify-between rounded-lg hover:bg-gray-50"
+              <div
+                :class="{
+                  'sm:pb-0': index !== navigationElement.children.length - 1,
+                }"
+                class="relative grid gap-6 bg-white px-3 py-2 sm:gap-6 sm:p-3"
               >
-                <div
-                  class="flex flex-col flex-grow pl-2"
-                  :class="{ 'max-w-200px': !!childElement.media }"
+                <RouterLink
+                  v-if="
+                    typeof childElement?.seoUrls?.[0]?.seoPathInfo !==
+                      'undefined'
+                  "
+                  :to="'/' + childElement?.seoUrls?.[0]?.seoPathInfo"
+                  class="flex justify-between rounded-lg hover:bg-gray-50 p-2"
                 >
-                  <p class="text-base font-medium text-gray-900">
+                  <div
+                    class="flex flex-col flex-grow pl-2"
+                    :class="{
+                      'max-w-200px md:max-w-300px': !!childElement.media,
+                    }"
+                  >
+                    <p class="text-base font-medium text-gray-900">
+                      {{ getTranslatedProperty(childElement, "name") }}
+                    </p>
+                    <p
+                      v-if="getTranslatedProperty(childElement, 'description')"
+                      class="mt-1 text-sm text-gray-500"
+                      v-html="
+                        getTranslatedProperty(childElement, 'description')
+                      "
+                    />
+                  </div>
+                  <div
+                    v-if="childElement.media"
+                    class="flex"
+                  >
+                    <img
+                      :src="childElement.media?.url"
+                      class="object-scale-down h-48 w-px-200 rounded-md"
+                      alt="Category image"
+                    >
+                  </div>
+                </RouterLink>
+                <div
+                  v-else
+                  class="px-4 py-2 sm:py-3"
+                >
+                  <p class="text-base font-medium text-gray-500">
                     {{ getTranslatedProperty(childElement, "name") }}
                   </p>
-                  <p
-                    v-if="getTranslatedProperty(childElement, 'description')"
-                    class="mt-1 text-sm text-gray-500"
-                    v-html="getTranslatedProperty(childElement, 'description')"
-                  />
                 </div>
-                <div v-if="childElement.media" class="flex">
-                  <img
-                    :src="childElement.media?.url"
-                    class="w-150px h-auto"
-                    alt="Category image"
-                  />
-                </div>
-              </RouterLink>
-            </div>
+              </div>
+            </template>
             <div
               class="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8"
             >
