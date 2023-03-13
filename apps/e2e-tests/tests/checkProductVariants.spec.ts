f932686bda46116f30b77e14665db3f439148b56
Krzysztof Stala <k.stala@shopware.com>
2023-03-13 17:12:19 +0000

    feat(e2e): add test for product variants (NTR)

---
+++ apps/e2e-tests/tests/checkProductVariants.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../page-objects/HomePage";
+import { ProductPage } from "../page-objects/ProductPage";
+import { CartPage } from "../page-objects/CartPage";
+
+test.describe.parallel.only("Check product variants", () => {
+  let homePage: HomePage;
+  let productPage: ProductPage;
+  let cartPage: CartPage;
+
+  // Before Hook
+  test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
+    productPage = new ProductPage(page);
+    cartPage = new CartPage(page);
+
+    await homePage.visitMainPage();
+  });
+
+  test("Add product variants to cart", async ({ page }) => {
+    await homePage.openVariantsCartPage();
+    await productPage.addVariantToCart();
+  });
+});
