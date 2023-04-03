33101dc172c842ce840f8b54cbdbd95636146c43
patzick <13100280+patzick@users.noreply.github.com>
2023-04-03 11:42:18 +0200

    fix: linter

--- packages/cms-base/components.d.ts
+++ packages/cms-base/components.d.ts
@@ -3,6 +3,10 @@ declare module "@vue/runtime-core" {
     CmsGenericBlock: typeof import("./components/cms/CmsGenericBlock.vue")["default"];
     CmsGenericElement: typeof import("./components/cms/CmsGenericElement.vue")["default"];
   };
+
+  declare interface AllowedComponentProps {
+    dataTestid?: string;
+  }
 }
 
 export {};
