ef73e1f31e287d96a9f2c3ed9310cfc0c22556ee
Björn Meyer <b.meyer@shopware.com>
2023-04-11 08:25:33 +0200

    fix(demo): update useAuthGuard, redirects (#95)

--- packages/cms-base/components/public/cms/element/CmsElementProductListing.vue
+++ packages/cms-base/components/public/cms/element/CmsElementProductListing.vue
@@ -36,9 +36,7 @@ setInitialListing(props?.content?.data?.listing);
 
 <template>
   <div class="bg-white">
-    <div
-      class="max-w-2xl mx-auto lg:max-w-full"
-    >
+    <div class="max-w-2xl mx-auto lg:max-w-full">
       <div v-if="getElements.length" class="mt-6">
         <div class="flex-1 flex-col justify-around">
           <SwProductCard
