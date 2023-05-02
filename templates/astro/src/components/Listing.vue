c03669f92c9cde7ed038adda9fadea1487185ac8
Björn Meyer <b.meyer@shopware.com>
2023-05-02 16:40:22 +0200

    feat: useListing with astro

---
+++ templates/astro/src/components/Listing.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useListing } from "@shopware-pwa/composables-next";
+
+const { search, getCurrentListing } = useListing({
+  listingType: "productSearchListing",
+});
+
+const listingSearch = async () => {
+  await search({ query: "ice cream" });
+  return getCurrentListing.value;
+};
+
+const listing = await listingSearch();
+</script>
+<template>
+  <div class="listing">
+    This Listing Page Example searches for "ice cream" products
+  </div>
+  <ul>
+    <li v-for="element in listing?.elements">
+      {{ element.name }}
+    </li>
+  </ul>
+</template>
