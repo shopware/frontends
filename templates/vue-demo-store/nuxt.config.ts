70936256fc289d5e53eea8a14bd81312415c9a1e
mkucmus <maciejkucmus@gmail.com>
2023-05-29 08:43:06 +0200

    chore: revert plugin

--- templates/vue-demo-store/nuxt.config.ts
+++ templates/vue-demo-store/nuxt.config.ts
@@ -1,5 +1,5 @@
 import transformerDirective from "@unocss/transformer-directives";
-// import type { PluginOption } from "vite";
+import type { PluginOption } from "vite";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
@@ -88,57 +88,57 @@ export default defineNuxtConfig({
       },
     ],
   },
-  // vite: {
-  //   plugins: [VueDisableInputsBeforeMount()],
-  // },
+  vite: {
+    plugins: [VueDisableInputsBeforeMount()],
+  },
 });
 
 // excuse me, just for tests
-// function VueDisableInputsBeforeMount(): PluginOption {
-//   return {
-//     name: "vite-vue-disable-inputs-before-mount",
-//     enforce: "pre",
-//     transform(code, id) {
-//       let newCode = code;
+function VueDisableInputsBeforeMount(): PluginOption {
+  return {
+    name: "vite-vue-disable-inputs-before-mount",
+    enforce: "pre",
+    transform(code, id) {
+      let newCode = code;
 
-//       if (id.endsWith(".vue")) {
-//         if (newCode.includes("<button") || newCode.includes("<select")) {
-//           if (newCode.includes(":disabled=")) {
-//             return null;
-//           }
-//           console.warn("transform", id.endsWith("vue"), id);
-//           newCode = newCode.replaceAll(
-//             "</script>",
-//             `
-//               const isDisabled = ref(typeof onMounted !=="undefined" ? true : false);
-//               if(typeof onMounted !=="undefined") {
-//                 onMounted(() => {
-//                   console.warn('enabling an input', '${id}');
-//                   isDisabled.value = false;
+      if (id.endsWith(".vue")) {
+        if (newCode.includes("<button") || newCode.includes("<select")) {
+          if (newCode.includes(":disabled=")) {
+            return null;
+          }
+          console.warn("transform", id.endsWith("vue"), id);
+          newCode = newCode.replaceAll(
+            "</script>",
+            `
+              const isDisabled = ref(typeof onMounted !=="undefined" ? true : false);
+              if(typeof onMounted !=="undefined") {
+                onMounted(() => {
+                  console.warn('enabling an input', '${id}');
+                  isDisabled.value = false;
 
-//                 });
-//               }
+                });
+              }
 
-//               </script>
-//               `
-//           );
-//           newCode = newCode.replaceAll(
-//             "<button",
-//             '<button :disabled="isDisabled" '
-//           );
-//           newCode = newCode.replaceAll(
-//             "<select",
-//             '<select :disabled="isDisabled" '
-//           );
-//         }
+              </script>
+              `
+          );
+          newCode = newCode.replaceAll(
+            "<button",
+            '<button :disabled="isDisabled" '
+          );
+          newCode = newCode.replaceAll(
+            "<select",
+            '<select :disabled="isDisabled" '
+          );
+        }
 
-//         if (id.endsWith("AccountLoginForm.vue")) {
-//           console.warn("AccountLoginForm.vue code", newCode);
-//         }
-//         return { code: newCode, id };
-//       }
+        if (id.endsWith("AccountLoginForm.vue")) {
+          console.warn("AccountLoginForm.vue code", newCode);
+        }
+        return { code: newCode, id };
+      }
 
-//       return null;
-//     },
-//   };
-// }
+      return null;
+    },
+  };
+}
