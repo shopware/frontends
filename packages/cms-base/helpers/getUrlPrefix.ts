46b33fbfdaf7210c93bf1f063f97a224a494f686
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2023-05-30 12:59:10 +0200

    feat: language switcher

--- packages/cms-base/helpers/getUrlPrefix.ts
+++ packages/cms-base/helpers/getUrlPrefix.ts
@@ -1,6 +1,5 @@
 export default function getUrlPrefix(url: string | any) {
   const prefix = inject("urlPrefix", "");
-  console.log("prefix", prefix);
   if (typeof url === "string") {
     return prefix ? `/${prefix}${url}` : url;
   }
