d56215a5a9af60d1c5b9a8f7c8b885f5ef38181f
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2023-06-01 15:37:13 +0200

    feat: language switcher

--- templates/vue-demo-store/i18n/src/helpers/prefix.ts
+++ templates/vue-demo-store/i18n/src/helpers/prefix.ts
@@ -10,7 +10,7 @@ import i18nConfig from "../config";
 export function getPrefix(locales: string[], name: string): string {
   if (name.includes(i18nConfig.defaultLocale)) return "";
   const index = locales.findIndex((element) => name.includes(element));
-  return index ? locales[index] : "";
+  return index >= 0 ? locales[index] : "";
 }
 
 /**
