77881bf5452b48cb3feca3c0039b52dcb7fdf641
Krzysiek Stala <krzysiek.stala@gmail.com>
2023-06-06 14:11:53 +0200

    feat: add plywright report artifacts (#185)

--- apps/e2e-tests/playwright.config.ts
+++ apps/e2e-tests/playwright.config.ts
@@ -20,6 +20,7 @@ console.log("Running tests for: ", baseURL);
 
 const config: PlaywrightTestConfig = {
   testDir: "./tests",
+  outputDir: "./reports",
   /* Maximum time one test can run for. */
   timeout: 90 * 1000,
   expect: {
