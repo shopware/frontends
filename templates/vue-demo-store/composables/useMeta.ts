853fab7544f1a0ac696adfd8a80b454457909793
Björn Meyer <b.meyer@shopware.com>
2023-06-13 09:41:13 +0200

    fix(demo): remove tmp fix useMeta (#251)

--- templates/vue-demo-store/composables/useMeta.ts
+++
@@ -1,6 +0,0 @@
-/**
- * TODO: Temp fix until new VueUse published:
- * - https://github.com/vueuse/vueuse/pull/2449
- * - https://github.com/vueuse/vueuse/actions/workflows/publish.yml
- */
-export const useMeta = useHead;
