c13f9b073f187737557a97fc95b9f951f61683a4
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2023-06-27 09:01:00 +0200

    feat: new dynamic api client base (#73)
    * feat: new dynamic api client
    
    * chore: fix build
    
    * chore: linter
    
    * chore: fix typings, added 6.4.20.0 definitions
    
    * chore: type fix
    
    * chore: temp ts parser removed
    
    * chore: lint error
    
    * feat: added 6.5.0.0 definitions
    
    * chore: lockfile
    
    * feat: session handler and example vite application
    
    * chore: remove not existing 6.5 instance
    
    * chore: fix build
    
    * cli to separate package
    
    * feat: fix broken pnpm-lock.yaml
    
    * feat: fix broken pnpm-lock.yaml
    
    * chore: package bump and typings fixes
    
    * fix tests
    
    * feat: patches for fixing schema
    
    * chore: add newest types
    
    * chore: proper typing for translations and generic fields

---
+++ packages/api-gen/build.config.ts
@@ -0,0 +1,10 @@
+import { defineBuildConfig } from "unbuild";
+export default defineBuildConfig({
+  entries: ["src/cli"],
+  rollup: {
+    inlineDependencies: true,
+  },
+  clean: true,
+  // declaration: true,
+  externals: [],
+});
