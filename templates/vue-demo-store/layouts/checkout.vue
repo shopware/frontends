72c9820931668d705bc2b7ddd43a70a6d56d6afe
mkucmus <maciejkucmus@gmail.com>
2023-06-30 14:49:49 +0200

    test: force e2e test

--- templates/vue-demo-store/layouts/checkout.vue
+++ templates/vue-demo-store/layouts/checkout.vue
@@ -4,7 +4,7 @@
   <div>
     <LayoutCheckoutHeader />
     <LayoutNotifications />
-    <div class="max-w-screen-xl mx-auto test">
+    <div class="max-w-screen-xl mx-auto test test2">
       <slot />
     </div>
   </div>
