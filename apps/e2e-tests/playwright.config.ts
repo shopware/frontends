34804cbbd77c45058d019ad688322fefe9c39587
mkucmus <maciejkucmus@gmail.com>
2023-06-30 14:29:22 +0200

    test: see flaky artifacts

--- apps/e2e-tests/playwright.config.ts
+++ apps/e2e-tests/playwright.config.ts
@@ -48,7 +48,7 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on-all-retries",
     testIdAttribute: "data-testid",
     baseURL,
   },
--- templates/vue-demo-store/layouts/checkout.vue
+++ templates/vue-demo-store/layouts/checkout.vue
@@ -4,7 +4,7 @@
   <div>
     <LayoutCheckoutHeader />
     <LayoutNotifications />
-    <div class="max-w-screen-xl mx-auto test">
+    <div class="max-w-screen-xl mx-auto">
       <slot />
     </div>
   </div>
