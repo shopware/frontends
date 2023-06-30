54f6230be29b4b13326c08ac8e28aa83d1959597
mkucmus <maciejkucmus@gmail.com>
2023-06-30 14:32:41 +0200

    test: trigger test

--- templates/vue-demo-store/layouts/checkout.vue
+++ templates/vue-demo-store/layouts/checkout.vue
@@ -4,7 +4,7 @@
   <div>
     <LayoutCheckoutHeader />
     <LayoutNotifications />
-    <div class="max-w-screen-xl mx-auto">
+    <div class="max-w-screen-xl mx-auto test">
       <slot />
     </div>
   </div>
