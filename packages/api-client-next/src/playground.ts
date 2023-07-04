57e4430f80b916f7e9f40fd905ea93536e49942a
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2023-07-04 13:30:37 +0200

    feat(api-client): exposed ApiRequestParams to use in predefined typings (#300)

--- packages/api-client-next/src/playground.ts
+++ packages/api-client-next/src/playground.ts
@@ -2,7 +2,7 @@
  * This file is just a temporary for playground and testing IDE support, it has no connections and will be removed, fell free to experiment here with the API
  */
 
-import { createAPIClient } from ".";
+import { RequestParameters, createAPIClient } from ".";
 import { operationPaths, operations } from "../api-types/apiTypes-6.4.19.0";
 
 const baseURL = "https://demo-frontends.shopware.store/store-api";
@@ -10,7 +10,7 @@ const accessToken = "SWSCBHFSNTVMAWNZDNFKSHLAYW";
 const apiType = "store-api";
 
 type extendedPaths = "qweqwe post /some/{addressId}/pathh" | operationPaths;
-type extendedOPerations = {
+type extendedOperations = {
   qweqwe: {
     parameters: {
       path: {
@@ -24,12 +24,18 @@ type extendedOPerations = {
   };
 } & operations;
 
-const apiInstance = createAPIClient<extendedOPerations, extendedPaths>({
+const apiInstance = createAPIClient<extendedOperations, extendedPaths>({
   baseURL,
   accessToken,
   apiType,
 });
-apiInstance.invoke("readSitemap get /sitemap", {});
+
+async function testing() {
+  const myResponse = await apiInstance.invoke(
+    "readCart get /checkout/cart?name",
+    {}
+  );
+}
 
 // async function customOperation() {
 //   const resp = await apiInstance.invoke("qweqwe post /some/{addressId}/pathh", {
@@ -42,3 +48,11 @@ apiInstance.invoke("readSitemap get /sitemap", {});
 //   const resp = await apiInstance.invoke("readSitemap get /sitemap", {});
 //   console.log("resp", resp);
 // }
+
+// Define method with predefined path
+const readCart = (params: RequestParameters<"readCart", operations>) =>
+  apiInstance.invoke("readCart get /checkout/cart?name", params);
+
+async function test() {
+  const resp = await readCart({});
+}
