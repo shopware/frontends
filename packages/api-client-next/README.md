589c09cdd9dee0db172c371afc5ecd740bdb4723
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2023-07-12 11:21:03 +0200

    feat(api-client): improved error handling (#316)

--- packages/api-client-next/README.md
+++ packages/api-client-next/README.md
@@ -115,6 +115,25 @@ async function loadMainNavigation() {
 }
 ```
 
+### Error handling
+
+Client is throwing `ApiClientError` with detailed information returned from the API. It will display clear message in the console or you can access `details` property to get raw information from the response.
+
+```typescript
+import { ApiClientError } from "@shopware/api-client";
+
+try {
+  // ... your request
+} catch (error) {
+  if (error instanceof ApiClientError) {
+    console.error(error); // This prints message summary
+    console.error("Details:", error.details); // Raw response from API
+  } else {
+    console.error("==>", error); // Another type of error, not recognized by API client
+  }
+}
+```
+
 ## Links
 
 - [📘 Documentation](https://frontends.shopware.com)
---
+++ packages/api-client-next/src/errorInterceptor.test.ts
@@ -0,0 +1,67 @@
+import { describe, expect, it } from "vitest";
+import { ApiError, ApiClientError, errorInterceptor } from "./errorInterceptor";
+import { FetchResponse } from "ofetch";
+
+describe("errorInterceptor", () => {
+  it("should extract different error messages from response to match snapshot", async () => {
+    const resp = {
+      _data: {
+        errors: [
+          {
+            title: "Constraint violation error",
+            detail:
+              'The "salutation" entity with id "1b341372add24e318d4aa33564245d67" does not exist.',
+            source: {
+              pointer: "/salutationId",
+            },
+          },
+          {
+            title: "Constraint violation error",
+            detail: "The value you selected is not a valid choice.",
+            source: {
+              pointer: "/storefrontUrl",
+            },
+          },
+          {
+            title: "Constraint violation error",
+            detail:
+              'The "salutation" entity with id "1b341372add24e318d4aa33564245d67" does not exist.',
+            source: {
+              pointer: "/billingAddress/salutationId",
+            },
+          },
+          {
+            code: "0",
+            status: "500",
+            title: "Internal Server Error",
+            detail: "Empty ids provided in criteria",
+          },
+        ],
+      },
+    } as unknown as FetchResponse<{
+      errors: Array<ApiError>;
+    }>;
+
+    await expect(() =>
+      errorInterceptor(resp),
+    ).toThrowErrorMatchingInlineSnapshot(
+      `
+      "Failed request
+       - [Constraint violation error][/salutationId] The \\"salutation\\" entity with id \\"1b341372add24e318d4aa33564245d67\\" does not exist.
+       - [Constraint violation error][/storefrontUrl] The value you selected is not a valid choice.
+       - [Constraint violation error][/billingAddress/salutationId] The \\"salutation\\" entity with id \\"1b341372add24e318d4aa33564245d67\\" does not exist.
+       - [Internal Server Error] Empty ids provided in criteria"
+    `,
+    );
+
+    try {
+      errorInterceptor(resp);
+    } catch (error) {
+      expect(error instanceof ApiClientError).toBe(true);
+
+      if (error instanceof ApiClientError) {
+        expect(error.details.errors.length).toBe(resp!._data!.errors.length);
+      }
+    }
+  });
+});
