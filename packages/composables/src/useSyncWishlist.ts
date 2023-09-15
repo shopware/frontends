3a3d38e804a1c7564eaa60694a642d4df0aedd6b
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2023-09-15 09:06:20 +0200

    fix: catch wishlist items 404

--- packages/composables/src/useSyncWishlist.ts
+++ packages/composables/src/useSyncWishlist.ts
@@ -6,6 +6,7 @@ import {
   mergeWishlistProducts as mergeWishlistProductsAPI,
 } from "@shopware-pwa/api-client";
 import { useShopwareContext } from "./useShopwareContext";
+import { ClientApiError } from "@shopware-pwa/types";
 
 export type UseSyncWishlistReturn = {
   /**
@@ -57,10 +58,17 @@ export function useSyncWishlist(): UseSyncWishlistReturn {
    * Only for logged-in users
    */
   async function getWishlistProducts() {
-    const response = await getWishlistProductsAPI(undefined, apiInstance);
-    _wishlistItems.value = [
-      ...response.products.elements.map((element) => element.id),
-    ];
+    try {
+      const response = await getWishlistProductsAPI(undefined, apiInstance);
+      _wishlistItems.value = [
+        ...response.products.elements.map((element) => element.id),
+      ];
+    } catch (e) {
+      const error = e as ClientApiError;
+      // If 404 ignore printing error and reset wishlist
+      if (error?.statusCode !== 404) console.error(error);
+      _wishlistItems.value = [];
+    }
   }
 
   async function mergeWishlistProducts(itemsToMerge: string[]) {
