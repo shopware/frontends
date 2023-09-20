ddd61cf17b2e2b35058e76a3648894f0859f9f6b
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2023-09-20 08:55:13 +0200

    fix(vue-demo): product listing structure

--- packages/cms-base/components/public/cms/section/CmsSectionSidebar.vue
+++ packages/cms-base/components/public/cms/section/CmsSectionSidebar.vue
@@ -15,24 +15,24 @@ const mobileBehavior = computed(() => props.content.mobileBehavior);
 </script>
 
 <template>
-  <div class="cms-section-sidebar flex flex-col md:block">
+  <div class="cms-section-sidebar grid grid-cols-12 md:grid">
+    <div class="col-span-12 md:col-span-9 order-2 md:order-2">
+      <CmsGenericBlock
+        v-for="cmsBlock in mainBlocks"
+        class="overflow-auto"
+        :key="cmsBlock.id"
+        :content="cmsBlock"
+      />
+    </div>
     <div
       :class="{
-        'inline-block align-top w-12/12 md:w-3/12 order-2 md:order-1':
+        'align-top col-span-12 md:col-span-3 order-1 md:order-1':
           mobileBehavior !== 'hidden',
         'hidden md:block': mobileBehavior === 'hidden',
       }"
     >
       <CmsGenericBlock
         v-for="cmsBlock in sidebarBlocks"
-        class="overflow-auto"
-        :key="cmsBlock.id"
-        :content="cmsBlock"
-      />
-    </div>
-    <div class="inline-block w-12/12 md:w-9/12 order-1 md:order-2">
-      <CmsGenericBlock
-        v-for="cmsBlock in mainBlocks"
         class="overflow-auto"
         :key="cmsBlock.id"
         :content="cmsBlock"
