2c6d93eac5734de8989ad37459e5dd0018a648e8
Björn Meyer <b.meyer@shopware.com>
2023-10-09 13:56:14 +0200

    fix(docs): adding security headers

--- apps/docs/src/vite.config.ts
+++ apps/docs/src/vite.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [SearchPlugin()],
+  server: {
+    headers: {
+      "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
+      "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
+      "X-Frame-Options": "DENY", // Stops your site being used as an iframe
+      "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
+    },
+  },
 });
