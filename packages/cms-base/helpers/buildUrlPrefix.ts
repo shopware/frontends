f3be470b42b536ba84a9ce968f440f9d6409bc19
Maciej D <7597086+mdanilowicz@users.noreply.github.com>
2023-10-23 19:43:48 +0200

    fix(cms): add category link resolver (#420)

--- packages/cms-base/helpers/buildUrlPrefix.ts
+++ packages/cms-base/helpers/buildUrlPrefix.ts
@@ -1,4 +1,6 @@
 export default function buildUrlPrefix(url: string | any, prefix: string) {
+  url = url[0] !== "/" ? `/${url}` : url;
+
   if (typeof url === "string") {
     return prefix ? `/${prefix}${url}` : url;
   }
