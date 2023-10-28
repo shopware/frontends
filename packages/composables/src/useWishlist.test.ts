fb9f1fbd654d13c47e1837126fc2576f010c70ca
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2023-10-28 09:35:11 +0200

    feat: add useLandingSearch test

--- packages/composables/src/useWishlist.test.ts
+++ packages/composables/src/useWishlist.test.ts
@@ -1,8 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
 import { useWishlist } from "./useWishlist";
-import * as useUser from "./useUser";
 import { shallowMount } from "@vue/test-utils";
-import { computed } from "vue";
+import { useUser } from "./useUser";
 
 const Component = {
   template: "<div/>",
@@ -37,36 +36,53 @@ const getMockProvide = () => ({
   },
 });
 
-describe("useWishlist", () => {
+vi.mock("./useLocalWishlist.ts", () => ({
+  useLocalWishlist() {
+    return {
+      getWishlistProducts: () => undefined,
+      items: { value: ["local-testId333"] },
+      clearWishlist: () => undefined,
+    };
+  },
+}));
+
+vi.mock("./useSyncWishlist.ts", () => ({
+  useSyncWishlist() {
+    return {
+      getWishlistProducts: () => undefined,
+      items: { value: ["sync-testId333"] },
+      mergeWishlistProducts: (items) => undefined,
+      removeFromWishlistSync: () => undefined,
+    };
+  },
+}));
+
+vi.mock("./useUser.ts");
+
+describe("useWishlist - not logged in user", () => {
+  useUser.mockImplementation(() => ({
+    isLoggedIn: { value: false },
+    isGuestSession: { value: false },
+  }));
+
   const wrapper = shallowMount(Component, getMockProvide());
 
-  vi.spyOn(useUser, "useUser").mockImplementation((): any => {
-    return {
-      isLoggedIn: computed(() => true),
-      isGuestSession: computed(() => false),
-    };
+  it("mergeWishlistProducts", async () => {
+    expect(wrapper.vm.items.length).toBe(1);
+    expect(wrapper.vm.count).toBe(1);
+    await wrapper.vm.clearWishlist();
+    await wrapper.vm.getWishlistProducts();
+    await wrapper.vm.mergeWishlistProducts();
   });
+});
 
-  vi.mock("./useLocalWishlist.ts", () => ({
-    useLocalWishlist() {
-      return {
-        getWishlistProducts: () => undefined,
-        items: { value: ["local-testId333"] },
-        clearWishlist: () => undefined,
-      };
-    },
+describe("useWishlist - logged in user", () => {
+  useUser.mockImplementation(() => ({
+    isLoggedIn: { value: true },
+    isGuestSession: { value: false },
   }));
 
-  vi.mock("./useSyncWishlist.ts", () => ({
-    useSyncWishlist() {
-      return {
-        getWishlistProducts: () => undefined,
-        items: { value: ["sync-testId333"] },
-        mergeWishlistProducts: (items) => undefined,
-        removeFromWishlistSync: () => undefined,
-      };
-    },
-  }));
+  const wrapper = shallowMount(Component, getMockProvide());
 
   it("mergeWishlistProducts", async () => {
     expect(wrapper.vm.items.length).toBe(1);
