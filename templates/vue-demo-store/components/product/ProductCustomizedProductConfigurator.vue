a27a2e7b455c9581370be5fef9354eab3bcad593
mkucmus <maciejkucmus@gmail.com>
2023-11-08 14:10:06 +0100

    feat: allow to upload images only

--- templates/vue-demo-store/components/product/ProductCustomizedProductConfigurator.vue
+++ templates/vue-demo-store/components/product/ProductCustomizedProductConfigurator.vue
@@ -80,7 +80,7 @@ const removeUploadedImage = (optionId: string) => {
             class="flex items-center justify-center w-full mt-4"
           >
             <label
-              for="dropzone-file"
+              :for="`dropzone-file-${option.id}`"
               class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
             >
               <div class="flex flex-col items-center justify-center pt-5 pb-6">
@@ -104,13 +104,14 @@ const removeUploadedImage = (optionId: string) => {
                   drop
                 </p>
                 <p class="text-xs text-gray-500 dark:text-gray-400">
-                  SVG, PNG, JPG or GIF (Max. 1 file(s), max. 10 MB per file)
+                  SVG, PNG, JPG or GIF (Max. 1 file, max. 10 MB)
                 </p>
               </div>
               <input
-                id="dropzone-file"
+                :id="`dropzone-file-${option.id}`"
                 type="file"
                 class="hidden"
+                accept="image/*"
                 @change="handleFileUpload($event, option.id)"
               />
             </label>
