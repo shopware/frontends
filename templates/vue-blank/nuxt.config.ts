ff1d1002681ad7ca590405de3ba64c191dcda85b
patzick <13100280+patzick@users.noreply.github.com>
2023-11-28 17:46:40 +0100

    refactor: vue-blank template transformation

--- templates/vue-blank/nuxt.config.ts
+++ templates/vue-blank/nuxt.config.ts
@@ -1,14 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  extends: ["@shopware-pwa/composables-next", "@shopware-pwa/cms-base"],
+  extends: [
+    "@shopware-pwa/composables-next/nuxt-layer",
+    "@shopware-pwa/cms-base",
+  ],
   runtimeConfig: {
     public: {
       shopware: {
-        shopwareEndpoint: "https://demo-frontends.shopware.store",
+        shopwareEndpoint: "https://demo-frontends.shopware.store/store-api/",
         shopwareAccessToken: "SWSCBHFSNTVMAWNZDNFKSHLAYW",
       },
     },
   },
+  modules: ["@shopware-pwa/nuxt3-module"],
   /**
    * Commented because of the StackBlitz error
    * Issue: https://github.com/shopware/frontends/issues/88
