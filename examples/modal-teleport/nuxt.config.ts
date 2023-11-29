acd081736d18c6bc84d7dcff4df48f200de62bbc
patzick <13100280+patzick@users.noreply.github.com>
2023-11-29 17:24:40 +0100

    chore: bump dependencies

--- examples/modal-teleport/nuxt.config.ts
+++ examples/modal-teleport/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@vueuse/nuxt", "@nuxt/devtools", "@unocss/nuxt"],
+  modules: ["@vueuse/nuxt", "@unocss/nuxt"],
   typescript: {
     strict: true,
   },
