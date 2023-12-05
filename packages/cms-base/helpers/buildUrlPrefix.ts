68e28891be24e8edd4497684e18447a16df72d12
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2023-12-05 16:53:51 +0100

    fix(vue-demo): external category link

--- packages/cms-base/helpers/buildUrlPrefix.ts
+++ packages/cms-base/helpers/buildUrlPrefix.ts
@@ -1,5 +1,8 @@
+import { urlIsAbsolute } from "@shopware-pwa/helpers-next";
+
 export default function buildUrlPrefix(url: string | any, prefix: string) {
   if (typeof url === "string") {
+    if (urlIsAbsolute(url)) return url;
     url = url[0] !== "/" ? `/${url}` : url;
     return prefix ? `/${prefix}${url}` : url;
   }
