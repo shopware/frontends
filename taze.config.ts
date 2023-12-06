0bf04bfef595b5b25d64d04883b6dd6c2ac557b0
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2023-12-06 13:57:05 +0100

    chore: bump dependencies (#491)

--- taze.config.ts
+++ taze.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   // write to package.json
   write: true,
   // run `npm install` or `yarn install` right after bumping
-  install: true,
+  install: false,
   mode: "major",
   // override with different bumping mode for each package
   packageMode: {
