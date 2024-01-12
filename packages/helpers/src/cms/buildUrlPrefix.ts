73477b440c9f775645b4d5a0ca3cea81c641e74b
Björn Meyer <b.meyer@shopware.com>
2024-01-12 12:05:33 +0100

    feat(cms-base): fix seoUrl bug

--- packages/helpers/src/cms/buildUrlPrefix.ts
+++ packages/helpers/src/cms/buildUrlPrefix.ts
@@ -1,12 +1,13 @@
 import { urlIsAbsolute } from "../urlIsAbsolute";
-// @ts-ignore-error: Get rid of any and use a proper object type
-export function buildUrlPrefix(url: string | any, prefix: string) {
+export function buildUrlPrefix(url: string | object, prefix: string) {
   if (typeof url === "string") {
     if (urlIsAbsolute(url)) return url;
     url = url[0] !== "/" ? `/${url}` : url;
     return prefix ? `/${prefix}${url}` : url;
   }
+  // @ts-expect-error: Get rid of any and use a proper object type
   if (url.path && prefix) {
+    // @ts-expect-error: Get rid of any and use a proper object type
     url.path = `/${prefix}${url.path}`;
   }
 
