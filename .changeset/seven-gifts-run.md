37c03c8ecda7bf512812bd1b4907acfe02921475
Björn Meyer <b.meyer@shopware.com>
2024-01-24 13:57:05 +0100

    fix(cms-base): getOptionsFromNode

---
+++ .changeset/seven-gifts-run.md
@@ -0,0 +1,5 @@
+---
+"@shopware-pwa/cms-base": minor
+---
+
+Make helper getOptionsFromNode more reliable, add error stack
--- packages/cms-base/helpers/html-to-vue/getOptionsFromNode.ts
+++ packages/cms-base/helpers/html-to-vue/getOptionsFromNode.ts
@@ -22,32 +22,47 @@ export function getOptionsFromNode(node: any): Options {
   let classNames = undefined;
   let align = undefined;
 
-  if (node.attrs.style && node.attrs.style !== "") {
-    style = node.attrs.style;
-    delete node.attrs.style; // we delete the nodes otherwise it would be added to rest again
+  try {
+    if (Object.keys(node).length === 0) {
+      return {};
+    }
+
+    if (Object.keys(node.attrs).length > 0) {
+      if (node.attrs.style && node.attrs.style !== "") {
+        style = node.attrs.style;
+        delete node.attrs.style; // we delete the nodes otherwise it would be added to rest again
+      }
+
+      if (node.attrs.class && node.attrs.class !== "") {
+        classNames = node.attrs.class;
+        delete node.attrs.class;
+      }
+
+      if (node.attrs.align && node.attrs.align !== "") {
+        align = node.attrs.align;
+        delete node.attrs.align;
+      }
+    }
+
+    const attrs =
+      Object.keys(node.attrs).length === 0 ? "undefined" : { ...node.attrs };
+
+    // Resolve URL if exist
+    if (attrs?.href) {
+      const { resolveUrl } = useUrlResolver();
+      attrs.href = `${resolveUrl(attrs.href)}`;
+    }
+
+    return {
+      ...(typeof align !== "undefined" && { align }),
+      ...(attrs !== "undefined" && typeof attrs !== "undefined" && { attrs }),
+      ...(typeof classNames !== "undefined" && { class: classNames }),
+      ...(typeof style !== "undefined" && { style }),
+    };
+  } catch (e) {
+    console.error("Error in getOptionsFromNode", e);
+    console.error(new Error().stack);
   }
 
-  if (node.attrs.class && node.attrs.class !== "") {
-    classNames = node.attrs.class;
-    delete node.attrs.class;
-  }
-
-  if (node.attrs.align && node.attrs.align !== "") {
-    align = node.attrs.align;
-    delete node.attrs.align;
-  }
-  const attrs = Object.keys(node.attrs).length === 0 ? null : { ...node.attrs };
-
-  // Resolve URL if exist
-  if (attrs?.href) {
-    const { resolveUrl } = useUrlResolver();
-    attrs.href = `${resolveUrl(attrs.href)}`;
-  }
-
-  return {
-    ...(typeof align != "undefined" && { align }),
-    ...(typeof attrs != "undefined" && { attrs }),
-    ...(typeof classNames != "undefined" && { class: classNames }),
-    ...(typeof style != "undefined" && { style }),
-  };
+  return {};
 }
