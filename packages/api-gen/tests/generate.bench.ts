92ecd8430c484eb47d2e7343916cc935b7d45eda
patzick <13100280+patzick@users.noreply.github.com>
2024-01-31 16:25:55 +0100

    chore: skip io task for now

--- packages/api-gen/tests/generate.bench.ts
+++ packages/api-gen/tests/generate.bench.ts
@@ -2,7 +2,7 @@ import { describe, bench } from "vitest";
 import { generate } from "../src/commands/generate";
 
 describe("api-gen - generate", () => {
-  bench("[api-gen][generate] - generate schema command", async () => {
+  bench.skip("[api-gen][generate] - generate schema command", async () => {
     await generate({
       cwd: __dirname,
       filename: "testSchema.json",
