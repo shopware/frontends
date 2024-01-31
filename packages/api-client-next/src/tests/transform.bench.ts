abe1f7c6c69debc8823482b95a73aa1d68c197ea
patzick <13100280+patzick@users.noreply.github.com>
2024-01-31 14:21:32 +0100

    chore: better naming for bench

--- packages/api-client-next/src/tests/transform.bench.ts
+++ packages/api-client-next/src/tests/transform.bench.ts
@@ -2,40 +2,43 @@ import { describe, bench } from "vitest";
 import { transformPathToQuery } from "../transformPathToQuery";
 
 describe("transforming requests", () => {
-  bench("transform without any params", () => {
+  bench("transformPathToQuery - transform without any params", () => {
     transformPathToQuery("readCart get /checkout/cart", {});
   });
 
-  bench("transform with query params", () => {
+  bench("transformPathToQuery - transform with query params", () => {
     transformPathToQuery("readCart get /checkout/cart?name", {
       name: "myId123",
     });
   });
 
-  bench("transform with body params", () => {
+  bench("transformPathToQuery - transform with body params", () => {
     transformPathToQuery("readCart get /checkout/cart", {
       name: "myId123",
     });
   });
 
-  bench("transform with path params", () => {
+  bench("transformPathToQuery - transform with path params", () => {
     transformPathToQuery("readCart get /checkout/cart/{name}", {
       name: "myId123",
     });
   });
 
-  bench("transform with header params", () => {
+  bench("transformPathToQuery - transform with header params", () => {
     transformPathToQuery("readCart get /checkout/cart name", {
       name: "myId123",
     });
   });
 
-  bench("transform with path,query,body,header params", () => {
-    transformPathToQuery("readCart get /checkout/cart/{id}?name surname", {
-      id: "myId123",
-      name: "myId123",
-      surname: "myOtherId",
-      bodyParam: "some123",
-    });
-  });
+  bench(
+    "transformPathToQuery - transform with path,query,body,header params",
+    () => {
+      transformPathToQuery("readCart get /checkout/cart/{id}?name surname", {
+        id: "myId123",
+        name: "myId123",
+        surname: "myOtherId",
+        bodyParam: "some123",
+      });
+    },
+  );
 });
