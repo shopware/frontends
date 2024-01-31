b4036be4ee34ae468459a4162bb990d74a4630c5
patzick <13100280+patzick@users.noreply.github.com>
2024-01-31 20:18:25 +0100

    chore: create api client bench

---
+++ packages/api-client-next/src/tests/apiClient.bench.ts
@@ -0,0 +1,17 @@
+import { describe, bench } from "vitest";
+import { createAPIClient, createAdminAPIClient } from "..";
+
+describe("apiClient benchmarks", () => {
+  bench("[api-client][createAPIClient] - creating client", async () => {
+    createAPIClient({
+      baseURL: "https://example.com",
+      accessToken: "123",
+    });
+  });
+
+  bench("[api-client][createAdminAPIClient] - creating client", async () => {
+    createAdminAPIClient({
+      baseURL: "https://example.com",
+    });
+  });
+});
