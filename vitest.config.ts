a90d25f18823b1ad0e80fcb1027315800359f902
patzick <13100280+patzick@users.noreply.github.com>
2024-01-31 16:10:33 +0100

    Revert "chore: test running all at once"
    This reverts commit 7386b14f6a069a1602c090b06d63d6d4f9ccb819.

--- vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vitest/config";
-import codspeedPlugin from "@codspeed/vitest-plugin";
-
-// config to run benchmarks globally
-export default defineConfig({
-  plugins: [codspeedPlugin()],
-});
