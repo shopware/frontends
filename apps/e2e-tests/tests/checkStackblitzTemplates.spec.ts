827c9838b9c6a59b89003bf03a82a011de2dc6cd
Krzysiek Stala <krzysiek.stala@gmail.com>
2024-02-05 19:41:59 +0100

    fix(e2e): stackblitz template tests fixes (#565)

--- apps/e2e-tests/tests/checkStackblitzTemplates.spec.ts
+++ apps/e2e-tests/tests/checkStackblitzTemplates.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from "@playwright/test";
 import { HomePage } from "../page-objects/HomePage";
 
 test("Verify stackblitz template", async ({ page }) => {
-  test.setTimeout(120000);
+  test.setTimeout(200000);
   let homePage: HomePage;
   const response = await page.goto("");
   homePage = new HomePage(page);
   await page.waitForLoadState();
-  await page.locator("text=Open in New Tab").click();
+  await page.waitForSelector("text=Open in New Tab");
   await page.waitForLoadState("domcontentloaded");
   await homePage.wait(10000);
 
