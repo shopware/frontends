892e5da81b3a837e4473f88b34e3c828a5ea6a47
Björn Meyer <b.meyer@shopware.com>
2024-02-20 14:39:56 +0100

    feat(demo): add limit select

--- templates/vue-demo-store/components/shared/SharedPagination.vue
+++ templates/vue-demo-store/components/shared/SharedPagination.vue
@@ -9,16 +9,13 @@ defineEmits<{
 }>();
 </script>
 <template>
-  <div
-    v-if="total > 1"
-    class="bg-white justify-center px-4 py-3 flex border-t border-secondary-200 sm:px-6"
-  >
+  <div v-if="total > 1">
     <nav
       class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
       aria-label="Pagination"
     >
       <button
-        v-if="current - 1 >= 1"
+        v-if="current - 1 >= 2"
         class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-secondary-300 bg-white text-sm font-medium text-secondary-500 hover:bg-secondary-50"
         @click="$emit('changePage', current - 1)"
       >
@@ -44,6 +41,9 @@ defineEmits<{
         v-if="current > 1"
         aria-current="page"
         class="bg-white border-secondary-300 text-secondary-500 hover:bg-secondary-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+        :class="[
+          current == 2 ? 'rounded-l-md border border-secondary-300' : '',
+        ]"
         @click="$emit('changePage', current - 1)"
       >
         {{ current - 1 }}
@@ -51,6 +51,10 @@ defineEmits<{
       <button
         aria-current="page"
         class="bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+        :class="[
+          current - 1 >= 1 ? '' : 'rounded-l-md border border-secondary-300',
+          total == current ? 'rounded-r-md border border-secondary-300' : '',
+        ]"
       >
         {{ current }}
       </button>
@@ -58,6 +62,11 @@ defineEmits<{
         v-if="current < total"
         aria-current="page"
         class="bg-white border-secondary-300 text-secondary-500 hover:bg-secondary-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+        :class="[
+          total == current + 1
+            ? 'rounded-r-md border border-secondary-300'
+            : '',
+        ]"
         @click="$emit('changePage', current + 1)"
       >
         {{ current + 1 }}
@@ -78,7 +87,7 @@ defineEmits<{
       <button
         v-if="total > current + 1"
         class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-secondary-300 bg-white text-sm font-medium text-secondary-500 hover:bg-secondary-50"
-        @click="$emit('changePage', total)"
+        @click="$emit('changePage', current + 1)"
       >
         <span class="sr-only">Next</span>
         <!-- Heroicon name: solid/chevron-right -->
