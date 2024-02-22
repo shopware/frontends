a0632c714dc41ef5b6f8557a025fde5ba32cd717
Kstala <krzysiek.stala@gmail.com>
2024-02-22 22:49:49 +0100

    feat: using stackblitzSDK to open templates

--- apps/e2e-tests/tests/checkStackblitzTemplates.spec.ts
+++ apps/e2e-tests/tests/checkStackblitzTemplates.spec.ts
@@ -1,26 +1,43 @@
 import { test, expect } from "@playwright/test";
-import { HomePage } from "../page-objects/HomePage";
+import fs from "fs";
+import path from "path";
 
-test("Verify stackblitz template", async ({ page }) => {
-  test.setTimeout(200000);
-  let homePage: HomePage;
-  const response = await page.goto("");
-  homePage = new HomePage(page);
-  await page.waitForLoadState();
-  await page.waitForSelector("text=Open in New Tab");
-  await page.waitForLoadState("domcontentloaded");
-  await homePage.wait(10000);
+const directoryPath = path.join(__dirname, "../../../templates/");
 
-  const consoleLogs = [];
-  page.on("console", (msg) => {
-    if (msg.type() == "error") {
-      console.log(msg.text());
-      consoleLogs.push(msg.text());
-    }
-    expect(msg.type()).not.toBe("error");
-  });
+fs.readdirSync(directoryPath).forEach((template) => {
+  test(`Open ${template}`, async ({ page }) => {
+    test.setTimeout(90000);
+    await page.goto("/apps/e2e-tests/pages/blank.html");
+    await Promise.all([
+      page.waitForLoadState("networkidle"),
+      page.waitForLoadState("load"),
+      page.evaluate((template) => {
+        window.StackBlitzSDK.openGithubProject(
+          `shopware/frontends/tree/main/templates/${template}`,
+          {
+            clickToLoad: false,
+            newWindow: false,
+            origin: "https://stackblitz.com/",
+          },
+        );
+      }, template),
+    ]);
+    await page.waitForLoadState("load"),
+      await expect(page).toHaveURL(
+        `https://stackblitz.com/github/shopware/frontends/tree/main/templates/${template}?file=README.md`,
+      );
 
-  page.on("response", (response) => {
-    expect(response.status()).toBe(200);
+    const consoleLogs = [];
+    page.on("console", (msg) => {
+      if (msg.type() == "error") {
+        console.log(msg.text());
+        consoleLogs.push(msg.text());
+      }
+      expect(msg.type()).not.toBe("error");
+    });
+
+    page.on("response", (response) => {
+      expect(response.status()).toBe(200);
+    });
   });
 });
