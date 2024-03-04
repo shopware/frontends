c9cc9293e4f380fdc1c5e199d3a3c091342c967d
mkucmus <maciejkucmus@gmail.com>
2024-03-04 12:42:05 +0100

    fix(apps): revert assertion

--- apps/e2e-tests/tests/checkCodeExamples.spec.ts
+++ apps/e2e-tests/tests/checkCodeExamples.spec.ts
@@ -18,7 +18,7 @@ fs.readdirSync(directoryPath)
   .filter((file) => !IGNORE_CHECK_EXAMPLES.includes(file))
   .forEach((file) => {
     test(`Verify ${file}`, async ({ page }) => {
-      const exampleName = `shopware/frontends/tree/feat/speed-up-stackblitz-examples/examples/${file}`;
+      const exampleName = `shopware/frontends/tree/main/examples/${file}`;
       await page.goto("file://" + __dirname + "/pages/blank.html", {
         waitUntil: "domcontentloaded",
         timeout: 0,
@@ -36,7 +36,7 @@ fs.readdirSync(directoryPath)
       expect(page.url()).toContain(
         `https://stackblitz.com/github/${exampleName}`,
       );
-      expect(await page.title()).toContain("StackBlitz");
+      expect(await page.title()).toContain("Frontends");
       await page
         .locator('iframe[title="Preview page"]')
         .waitFor({ state: "visible" });
