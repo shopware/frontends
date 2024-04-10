1dfc3c41a2ae2caeeb085ac47dd6d777d1f50d84
Maciek Kucmus <maciejkucmus@gmail.com>
2024-04-10 09:18:43 +0200

    fix(examples): add required container for e2e purposes (#862)

--- examples/b2b-quote-management/src/App.vue
+++ examples/b2b-quote-management/src/App.vue
@@ -11,7 +11,7 @@ const chosenQuote = ref(null);
 refreshSessionContext();
 </script>
 <template>
-  <div class="container mx-auto">
+  <div class="test-wrapper container mx-auto">
     <ul class="my-10 text-2xl flex gap-10 justify-center">
       <li><router-link to="/quotes">Quotes</router-link></li>
       <li><router-link to="/request-quote">Request quote</router-link></li>
