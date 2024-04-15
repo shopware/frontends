c8319bed7d7e95a43d094c9d55b36812d1133208
Maciek Kucmus <maciejkucmus@gmail.com>
2024-04-15 10:59:11 +0200

    feat(examples): migrate use-checkout example to nuxt (#843)

---
+++ examples/use-checkout/nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ["@shopware-pwa/composables-next/nuxt-layer"],
+  modules: ["@shopware-pwa/nuxt3-module", "@unocss/nuxt"],
+  shopware: {
+    endpoint: "https://demo-frontends.shopware.store/store-api/",
+    accessToken: "SWSCBHFSNTVMAWNZDNFKSHLAYW",
+  },
+  css: [
+    "@unocss/reset/tailwind-compat.css", // needed to reset styles see https://unocss.dev/guide/style-reset (@unocss/reset)
+  ],
+  devtools: { enabled: true },
+});
