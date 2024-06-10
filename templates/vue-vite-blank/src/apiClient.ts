f7b201ba0c1e343d6c52447e8408e24e98e12d28
mkucmus <maciejkucmus@gmail.com>
2024-06-10 09:53:33 +0200

    feat(templates): new way of handling a context change for vue-vite-blank

--- templates/vue-vite-blank/src/apiClient.ts
+++ templates/vue-vite-blank/src/apiClient.ts
@@ -6,11 +6,4 @@ export const apiClient = createAPIClient<operations>({
   baseURL: import.meta.env.VITE_DEMO_API_URL,
   accessToken: import.meta.env.VITE_DEMO_API_ACCESS_TOKEN,
   contextToken: Cookies.get("sw-context-token"),
-  onContextChanged(newContextToken) {
-    Cookies.set("sw-context-token", newContextToken, {
-      expires: 365, // days
-      path: "/",
-      sameSite: "lax",
-    });
-  },
 });
--- templates/vue-vite-blank/src/main.ts
+++ templates/vue-vite-blank/src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-
+import Cookies from "js-cookie";
 import { createShopwareContext } from "@shopware-pwa/composables-next/lib";
 import { apiClient } from "./apiClient";
+
+apiClient.hook("onContextChanged", (newContextToken) => {
+  Cookies.set("sw-context-token", newContextToken, {
+    expires: 365, // days
+    path: "/",
+    sameSite: "lax",
+  });
+});
 
 const app = createApp(App);
 
