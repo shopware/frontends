bc78b99cb9fe92e24042a54c7b4d5752c744985d
Maciek Kucmus <maciejkucmus@gmail.com>
2024-06-10 23:07:57 +0200

    feat(examples): Amazon Pay Button (#992)
    * Delete examples/amazon-pay-button-example/.npmrc
    
    * fix: changes after CR
    
    ---------
    
    Co-authored-by: patzick <13100280+patzick@users.noreply.github.com>

---
+++ examples/amazon-pay-button-example/playground/nuxt.config.ts
@@ -0,0 +1,37 @@
+export default defineNuxtConfig({
+  extends: ["@shopware-pwa/composables-next/nuxt-layer"],
+  modules: ["@shopware-pwa/nuxt3-module", "../src/module"], // path to amazon-pay module
+  runtimeConfig: {
+    shopware: {
+      endpoint: "https://demo-frontends.shopware.store/store-api",
+      accessToken: "SWSCUJQXHJNQXKJAFVJFZLJQXH",
+    },
+    amazonPay: {
+      storeId:
+        "YOUR STORE ID HERE (e.g. amzn1.application-oa2-client.0123456789abcdef...)", // from amazon pay panel
+      publicKeyId: "YOUR PUBLIC KEY ID", // from amazon pay panel
+      merchantId: "YOUR MERCHANT ID", // from amazon pay panel
+      privateKeyPath:
+        "relative path to your private key (e.g ./server/private.pem)", // from amazon pay panel
+      region: "DE", // region of your store
+      sandbox: true, // whether to use the sandbox environment
+      algorithm: "AMZN-PAY-RSASSA-PSS-V2", // algorithm to use
+      webCheckoutDetails: {
+        checkoutReviewReturnUrl:
+          "PUT YOUR REVIEW URL HERE (e.g. https://frontends-demo.vercel.app/checkout)", // registered in amazon pay panel
+        checkoutResultReturnUrl:
+          "PUT YOUR RESULT URL HERE (e.g. https://frontends-demo.vercel.app/checkout/success)", // registered in amazon pay panel
+      },
+      shopwareAdminApi: {
+        endpoint:
+          "Shopware 6 Admin API endpoint (e.g https://demo-frontends.shopware.store/api)",
+        credentials: {
+          // credentials for the Shopware 6 Admin API
+          client_id: "SWIABTJARFASQLD5SGS0BVVOZG",
+          client_secret: "QUhPMHlPbF2Cvxnc2eG5XN1NCRDhBRU55TEhWV05qQm50MEowTXU",
+        },
+      },
+    },
+  },
+  devtools: { enabled: true },
+});
---
+++ examples/amazon-pay-button-example/src/runtime/server/api/amazon-pay/pay.post.ts
@@ -0,0 +1,78 @@
+import fs from "node:fs";
+import Client from "@amazonpay/amazon-pay-api-sdk-nodejs";
+import * as path from "node:path";
+import { z } from "zod";
+
+import { createError, useRuntimeConfig } from "#imports";
+
+import { defineEventHandler, readValidatedBody } from "h3";
+
+const paySchema = z.object({
+  sessionId: z.string().uuid(),
+  orderNumber: z.string(),
+  orderId: z.string(),
+  chargeAmount: z.object({
+    amount: z.string(),
+    currencyCode: z.string(),
+  }),
+});
+
+export default defineEventHandler(async (event) => {
+  const runtimeConfig = useRuntimeConfig();
+  const PUBLIC_KEY_ID = runtimeConfig.amazonPay?.publicKeyId;
+  const filePath =
+    runtimeConfig.amazonPay?.privateKeyPath ||
+    path.join(process.cwd(), "server", "private.pem");
+
+  const config = {
+    publicKeyId: PUBLIC_KEY_ID,
+    privateKey: fs.readFileSync(filePath, { encoding: "utf8" }),
+    region: runtimeConfig.amazonPay?.region,
+    sandbox: runtimeConfig.amazonPay?.sandbox,
+    algorithm: runtimeConfig.amazonPay?.algorithm || "AMZN-PAY-RSASSA-PSS-V2",
+  };
+  const parsedBody = await readValidatedBody(event, (body) =>
+    paySchema.safeParse(body),
+  );
+
+  if (!parsedBody.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Bad Request",
+      stack: "",
+      data: parsedBody.error.issues,
+    });
+  }
+
+  const payload = {
+    webCheckoutDetails: {
+      checkoutReviewReturnUrl:
+        runtimeConfig.amazonPay?.webCheckoutDetails.checkoutReviewReturnUrl,
+      // url from nuxt.config.ts (amazonPay?.webCheckoutDetails.checkoutResultReturnUrl) + orderId
+      checkoutResultReturnUrl:
+        runtimeConfig.amazonPay?.webCheckoutDetails.checkoutResultReturnUrl +
+        parsedBody.data.orderId,
+    },
+    paymentDetails: {
+      paymentIntent: "AuthorizeWithCapture",
+      chargeAmount: parsedBody.data.chargeAmount,
+      canHandlePendingAuthorization: false,
+    },
+    merchantMetadata: {
+      merchantReferenceId: parsedBody.data.orderNumber,
+      merchantStoreName: "Frontends Demo Store",
+      noteToBuyer: "",
+      customInformation: "",
+    },
+  };
+
+  const aPayClient = new Client.WebStoreClient(config);
+  const response = await aPayClient.updateCheckoutSession(
+    parsedBody.data.sessionId,
+    payload,
+  );
+
+  return {
+    result: response.data,
+  };
+});
