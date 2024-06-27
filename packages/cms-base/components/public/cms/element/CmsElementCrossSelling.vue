60b6fd99e94cabee7c73e609204af56aa333fa2c
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2024-06-27 13:01:29 +0200

    feat(cms): remove test classes

--- packages/cms-base/components/public/cms/element/CmsElementCrossSelling.vue
+++ packages/cms-base/components/public/cms/element/CmsElementCrossSelling.vue
@@ -50,7 +50,6 @@ const crossSellCollections = computed(() => {
 const { width } = useElementSize(crossSellContainer);
 const slidesToShow = computed(() => {
   const minWidth = +(config.value.minWidth?.value.replace(/\D+/g, "") || 0);
-  console.log("test", minWidth);
   return Math.floor(width.value / (minWidth * 1.2));
 });
 
