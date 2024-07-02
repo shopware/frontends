db42df4aef6fcb5113d058fa5821274b58077407
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2024-07-02 15:21:45 +0200

    fix(api-gen): better validation rule for COMPONENTS_API_ALIAS flag and proper overrides loading (#1082)
    * fix(api-gen): better validation rule for COMPONENTS_API_ALIAS flag
    
    * fix: correct overrides loading

---
+++ .changeset/orange-glasses-worry.md
@@ -0,0 +1,5 @@
+---
+"@shopware/api-gen": patch
+---
+
+`COMPONENTS_API_ALIAS` rule - additional message suggesting that the schema component name might be incorrect
--- packages/api-gen/src/validation-rules/componentsApiAlias.rule.ts
+++ packages/api-gen/src/validation-rules/componentsApiAlias.rule.ts
@@ -1,8 +1,8 @@
-import { snakeCase } from "scule";
+import { snakeCase, pascalCase } from "scule";
 import { equals } from "@vitest/expect";
 import { diff } from "@vitest/utils/diff";
 import c from "picocolors";
-import type { ObjectSubtype } from "openapi-typescript";
+import type { ObjectSubtype, StringSubtype } from "openapi-typescript";
 
 export default (componentName: string, body: ObjectSubtype) => {
   // aliast needs to be in snake case. Examples:
@@ -11,22 +11,23 @@ export default (componentName: string, body: ObjectSubtype) => {
   const properAliasDefinition = {
     type: "string",
     enum: [snakeCase(componentName)],
-  };
+  } as StringSubtype;
 
-  const bodyValue = body.properties?.apiAlias;
+  const bodyValue = body.properties?.apiAlias as ObjectSubtype;
 
   // skip if apiAlias is not defined
   if (!bodyValue) {
     return null;
   }
 
-  // check if apiAlias is required
-  if (body.required && !body.required.includes("apiAlias")) {
-    return `Component ${c.bold(componentName)} has invalid ${c.bold("apiAlias")} definition. This field should be required.`;
-  }
-
   const result = equals(properAliasDefinition, bodyValue);
   if (!result) {
+    let additionalMessage = "";
+    const bodyEnumValue = bodyValue?.enum?.[0] as string | undefined;
+    if (bodyEnumValue && properAliasDefinition.enum![0] !== bodyEnumValue) {
+      additionalMessage = `It's also possible, that the schema component name is not correct and apiApias is proper. In that case schema component name should be ${c.bold(pascalCase(bodyEnumValue))}. Confirm proper solution with the source code.`;
+    }
+
     return `Component ${c.bold(componentName)} has invalid ${c.bold("apiAlias")} definition. Diff:\n ${diff(
       properAliasDefinition,
       bodyValue,
@@ -34,7 +35,12 @@ export default (componentName: string, body: ObjectSubtype) => {
         aColor: c.green,
         bColor: c.red,
       },
-    )}`;
+    )}${additionalMessage.length ? `\n${additionalMessage}` : ""}`;
+  }
+
+  // check if apiAlias is required
+  if (!body.required?.includes("apiAlias")) {
+    return `Component ${c.bold(componentName)} has invalid ${c.bold("apiAlias")} definition. This field should be required.`;
   }
 
   return null;
