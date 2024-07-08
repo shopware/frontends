fd4c822ac3f6a7e09e5c419be29f00684c1f3034
mkucmus <maciejkucmus@gmail.com>
2024-07-08 13:14:13 +0200

    chore: deps change

--- packages/eslint-config-shopware/index.mjs
+++ packages/eslint-config-shopware/index.mjs
@@ -1,27 +1,13 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   {
     ignores: ["**/dist/", "**/node_modules/"],
   },
-  ...compat.extends("plugin:@typescript-eslint/recommended", "prettier"),
+  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
   {
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-
     languageOptions: {
       globals: {},
       ecmaVersion: "latest",
