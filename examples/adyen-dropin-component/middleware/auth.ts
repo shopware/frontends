58c79b34096ab64ab7dade7379c1804c5aff5771
Maciek Kucmus <maciejkucmus@gmail.com>
2024-08-08 10:45:01 +0200

    Delete examples/adyen-dropin-component/middleware/auth.ts

--- examples/adyen-dropin-component/middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-import { useNuxtApp, defineNuxtRouteMiddleware } from "#imports";
-
-export default defineNuxtRouteMiddleware((to, from) => {
-  const nuxtApp = useNuxtApp();
-
-  nuxtApp.$adyenCheckout;
-});
