75318747536b3cad5b83804a730b6680deb3fc8d
Maciek Kucmus <maciejkucmus@gmail.com>
2024-08-14 14:00:42 +0200

    fix(nuxt3-module): reading context cookie ssr (#1207)

--- packages/nuxt3-module/plugin.ts
+++ packages/nuxt3-module/plugin.ts
@@ -1,7 +1,6 @@
 import {
   defineNuxtPlugin,
   useRuntimeConfig,
-  // useCookie,
   useState,
   createShopwareContext,
   createError,
@@ -9,8 +8,21 @@ import {
 import { ref } from "vue";
 import Cookies from "js-cookie";
 import { createAPIClient } from "@shopware/api-client";
-
+import { getCookie } from "h3";
 import { isMaintenanceMode } from "@shopware-pwa/helpers-next";
+import type { ApiClient } from "#shopware";
+
+declare module "#app" {
+  interface NuxtApp {
+    $shopwareApiClient: ApiClient;
+  }
+}
+
+declare module "vue" {
+  interface ComponentCustomProperties {
+    $shopwareApiClient: ApiClient;
+  }
+}
 
 export default defineNuxtPlugin((NuxtApp) => {
   const runtimeConfig = useRuntimeConfig();
@@ -27,15 +39,19 @@ export default defineNuxtPlugin((NuxtApp) => {
   }
 
   const shouldUseSessionContextInServerRender =
-    !process.server ||
+    !NuxtApp.ssrContext ||
     !!runtimeConfig.public?.shopware?.useUserContextInSSR ||
     !!runtimeConfig?.shopware?.useUserContextInSSR;
+
+  const contextTokenFromCookie = !!NuxtApp.ssrContext
+    ? getCookie(NuxtApp.ssrContext?.event, "sw-context-token")
+    : Cookies.get("sw-context-token");
 
   const apiClient = createAPIClient({
     baseURL: shopwareEndpoint,
     accessToken: shopwareAccessToken,
     contextToken: shouldUseSessionContextInServerRender
-      ? Cookies.get("sw-context-token")
+      ? contextTokenFromCookie
       : "",
   });
 
@@ -73,159 +89,10 @@ export default defineNuxtPlugin((NuxtApp) => {
   NuxtApp.vueApp.provide("swSessionContext", sessionContextData);
   // in case someone tries to use it in nuxt specific code like middleware
   useState("swSessionContext", () => sessionContextData);
+
+  return {
+    provide: {
+      shopwareApiClient: apiClient as ApiClient,
+    },
+  };
 });
-
-// const ShopwarePlugin = {
-//   install(app, options) {
-//     const runtimeConfig = useRuntimeConfig();
-
-//     const shouldUseSessionContextInServerRender =
-//       !!runtimeConfig.public?.shopware?.useUserContextInSSR ||
-//       !options.isServer;
-
-//     const contextToken = getCookieContextToken();
-//     const languageId = getCookieLanguageId();
-
-//     const shopwareEndpoint =
-//       runtimeConfig?.shopware?.endpoint ||
-//       runtimeConfig.public.shopware.endpoint;
-
-//     if (
-//       !shopwareEndpoint ||
-//       !runtimeConfig.public?.shopware?.accessToken
-//     ) {
-//       throw new Error(
-//         "Make sure that shopwareEndpoint and shopwareAccessToken are settled in the configuration",
-//       );
-//     }
-
-//     const instance = createApiClientInstance(
-//       shopwareEndpoint,
-//       runtimeConfig.public.shopware.accessToken,
-//       getContextToken(shouldUseSessionContextInServerRender),
-//       getLanguageId(shouldUseSessionContextInServerRender),
-//     );
-
-//     /**
-//      * Save current contextToken when its change
-//      */
-//     instance.onConfigChange(({ config }) => {
-//       try {
-//         // only save cookies on client side render
-//         if (shouldUseSessionContextInServerRender) {
-//           contextToken.value = config.contextToken;
-//           languageId.value = config.languageId;
-//           setCookieContextToken(config.contextToken);
-//           setCookieLanguageId(config.languageId);
-//         }
-//       } catch (e) {
-//         // Sometimes cookie is set on server after request is send, it can fail silently
-//       }
-//     });
-//     const shopwareContext = createShopwareContext(app, {
-//       apiInstance: instance,
-//       enableDevtools: true,
-//       shopwareDefaults: options.apiDefaults,
-//       devStorefrontUrl: runtimeConfig.public.shopware?.devStorefrontUrl || null,
-//     });
-
-//     const apiClient = createAPIClient<operations, operationPaths>({
-//       baseURL: runtimeConfig.public.shopware.endpoint,
-//       accessToken: runtimeConfig.public.shopware.accessToken,
-//       contextToken: Cookies.get("sw-context-token"),
-//       onContextChanged(newContextToken) {
-//         Cookies.set("sw-context-token", newContextToken, {
-//           expires: 365, // days
-//           path: "/",
-//           sameSite: "lax",
-//         });
-//       },
-//     });
-
-//     app.provide("shopware", shopwareContext);
-//     const sessionContextData = ref();
-//     app.provide("swSessionContext", sessionContextData);
-//     app.provide("apiClient", apiClient);
-//     // in case someone tries to use it in nuxt specific code like middleware
-//     useState("swSessionContext", () => sessionContextData);
-//   },
-// };
-
-// function getCookieContextToken() {
-//   return useCookie("sw-context-token", {
-//     maxAge: 60 * 60 * 24 * 365,
-//     sameSite: "Lax",
-//     path: "/",
-//   });
-// }
-
-// function setCookieContextToken(contextToken) {
-//   Cookies.set("sw-context-token", contextToken || "", {
-//     expires: 365, //days
-//     sameSite: "Lax",
-//     path: "/",
-//   });
-// }
-
-// function getContextToken(shouldUseSessionContextInServerRender) {
-//   const contextToken = getCookieContextToken();
-//   // workaround for SSG case, where cookies contains additional dot in name, related: https://github.com/shopware/frontends/commit/ee5b8a71e1e016c973a7852efa3b85a136e6ea14
-//   const cookieContextToken = Cookies.get("sw-context-token");
-
-//   return shouldUseSessionContextInServerRender ? contextToken.value || cookieContextToken : "";
-// }
-
-// function getCookieLanguageId() {
-//   return useCookie("sw-language-id", {
-//     maxAge: 60 * 60 * 24 * 365,
-//     sameSite: "Lax",
-//     path: "/",
-//   });
-// }
-
-// function setCookieLanguageId(languageId) {
-//   Cookies.set("sw-language-id", languageId || "", {
-//     expires: 365, //days
-//     sameSite: "Lax",
-//     path: "/",
-//   });
-// }
-
-// function getLanguageId(shouldUseSessionContextInServerRender) {
-//   const languageId = getCookieLanguageId();
-//   // workaround for SSG case, where cookies contains additional dot in name, related: https://github.com/shopware/frontends/commit/ee5b8a71e1e016c973a7852efa3b85a136e6ea14
-//   const cookieLanguageId = Cookies.get("sw-language-id");
-
-//   return shouldUseSessionContextInServerRender ? languageId.value || cookieLanguageId : undefined;
-// }
-
-// /**
-//  * Server config has bigger prio than client
-//  */
-// function createApiClientInstance(
-//   shopwareEndpoint,
-//   shopwareAccessToken,
-//   contextToken,
-//   languageId,
-// ) {
-//   return createInstance({
-//     endpoint: shopwareEndpoint,
-//     accessToken: shopwareAccessToken,
-//     timeout: shopwareAccessToken || 10000,
-//     auth: {
-//       username:
-//         "<%= options.shopwareApiClient.auth ? options.shopwareApiClient.auth.username : undefined %>",
-//       password:
-//         "<%=  options.shopwareApiClient.auth ? options.shopwareApiClient.auth.password : undefined %>",
-//     },
-//     contextToken: contextToken,
-//     languageId: languageId,
-//   });
-// }
-
-// export default defineNuxtPlugin(async (nuxtApp) => {
-//   nuxtApp.vueApp.use(ShopwarePlugin, {
-//     apiDefaults: getDefaultApiParams(),
-//     isServer: !!nuxtApp.ssrContext,
-//   });
-// });
