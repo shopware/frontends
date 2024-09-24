f5ffa53558416c767d24df698d686b1ec382e24e
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2024-09-24 15:48:33 +0200

    feat: update packages

--- examples/b2b-employee-management/nuxt.config.ts
+++ examples/b2b-employee-management/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   extends: ["@shopware-pwa/composables-next/nuxt-layer"],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@shopware-pwa/nuxt3-module"],
+  modules: ["@shopware-pwa/nuxt3-module", "@unocss/nuxt"],
   shopware: {
     accessToken: "SWSCBHFSNTVMAWNZDNFKSHLAYW",
     endpoint: "https://demo-frontends.shopware.store/store-api/",
