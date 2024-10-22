6cb5e39db285f4060bb831d4a0ca55a5350bc85e
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2024-10-22 14:30:44 +0200

    feat: add tests

--- packages/composables/src/useBreadcrumbs/useBreadcrumbs.test.ts
+++ packages/composables/src/useBreadcrumbs/useBreadcrumbs.test.ts
@@ -1,8 +1,14 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, beforeEach, vi } from "vitest";
 import { useBreadcrumbs } from "./useBreadcrumbs";
 import { useSetup } from "../_test";
 
 describe("useBreadcrumbs", () => {
+  const consoleErrorSpy = vi.spyOn(console, "error");
+
+  beforeEach(() => {
+    consoleErrorSpy.mockImplementation(() => {});
+  });
+
   it("should add breadcrumbs", async () => {
     const { vm } = useSetup(() =>
       useBreadcrumbs([
@@ -50,5 +56,29 @@ describe("useBreadcrumbs", () => {
         },
       }),
     );
+  });
+
+  it("should invoke buildDynamicBreadcrumbs", async () => {
+    const { vm, injections } = useSetup(() => useBreadcrumbs());
+    injections.apiClient.invoke.mockResolvedValue({
+      data: {
+        breadcrumbs: [
+          {
+            path: "test",
+          },
+        ],
+      },
+    });
+    await vm.buildDynamicBreadcrumbs("123");
+
+    expect(vm.breadcrumbs[0].path).toBe("/test");
+  });
+
+  it("should log error when buildDynamicBreadcrumbs fails", async () => {
+    const { vm, injections } = useSetup(() => useBreadcrumbs());
+    injections.apiClient.invoke.mockRejectedValue(new Error("test"));
+    await vm.buildDynamicBreadcrumbs("123");
+
+    expect(consoleErrorSpy).toBeCalled();
   });
 });
