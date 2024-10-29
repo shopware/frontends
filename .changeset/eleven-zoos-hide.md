e18a4dba8eb9fc3e8dd8deca9edaa87ee6683268
Maciej D <7597086+mdanilowicz@users.noreply.github.com>
2024-10-29 09:59:06 +0100

    fix(vue-demo): accessibility issues (#1395)
    * fix(vue-demo): autocomplete values
    
    * fix(vue-demo): tag text color

---
+++ .changeset/eleven-zoos-hide.md
@@ -0,0 +1,12 @@
+---
+"vue-demo-store": patch
+---
+
+Fixed autocomplete attribute value for inputs
+
+- first-name -> given-name
+- last-name -> family-name
+- password -> current-password
+- Street -> street-address
+- zipcode -> postal-code
+- city -> address-level2
--- templates/vue-demo-store/components/product/ProductCard.vue
+++ templates/vue-demo-store/components/product/ProductCard.vue
@@ -105,7 +105,7 @@ const srcPath = computed(() => {
       <div class="absolute top-5 -left-1 z-10">
         <span
           v-if="product.markAsTopseller"
-          class="bg-[#FFBD5D] px-2.5 py-1.5 color-white text-xl"
+          class="bg-[#FFBD5D] px-2.5 py-1.5 color-black text-xl"
         >
           {{ $t("product.badges.topseller") }}
         </span>
