0052ee46d2e3a0462d438d5b0be1ee75c1936e90
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2024-11-06 08:29:11 +0100

    feat: roles init styles

--- examples/b2b-employee-management/pages/roles.vue
+++ examples/b2b-employee-management/pages/roles.vue
@@ -19,14 +19,46 @@ const updateRoles = async () => {
 <template>
   <div class="container mx-auto px-4">
     <h2>Roles</h2>
-    <NuxtLink to="/create-new-role">Create new role</NuxtLink>
+    <NuxtLink
+      to="/create-new-role"
+      class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+    >
+      Create new role
+    </NuxtLink>
+    <table v-if="roles.length" class="min-w-full bg-white">
+      <thead>
+        <tr>
+          <th
+            class="py-2 px-4 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-medium text-gray-600 uppercase tracking-wider"
+          >
+            Role
+          </th>
 
-    <ul v-if="roles.length">
-      <li v-for="role in roles" :key="role.id">
-        <NuxtLink :to="`/edit-role/${role.id}`"> {{ role.name }}</NuxtLink>
-        <button @click="handleDelete(role.id)">Delete</button>
-      </li>
-    </ul>
+          <th class="py-2 px-4 border-b border-gray-200 bg-gray-100"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="role in roles" :key="role.id" class="hover:bg-gray-100">
+          <td class="py-2 px-4 border-b border-gray-200">
+            {{ role.name }}
+          </td>
+
+          <td class="py-2 px-4 border-b border-gray-200">
+            <NuxtLink
+              :to="`/edit-role/${role.id}`"
+              class="text-blue-500 hover:text-blue-700"
+              >Edit</NuxtLink
+            >
+            <Button
+              @click="handleDelete(role.id)"
+              class="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+              >Delete</Button
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
     <div v-else>
       <p>
         No roles found, please
