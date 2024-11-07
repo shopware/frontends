dd52a6cd2219bdab37995a47dd1d22f73918132c
Kstala <krzysiek.stala@gmail.com>
2024-11-08 00:54:21 +0100

    chore: add accessibility tag

--- apps/e2e-tests/tests/accessibilityCheck.spec.ts
+++ apps/e2e-tests/tests/accessibilityCheck.spec.ts
@@ -4,36 +4,39 @@ import { HomePage } from "../page-objects/HomePage";
 import { CategoryPage } from "../page-objects/CategoryPage";
 import { ProductPage } from "../page-objects/ProductPage";
 
-test.describe
-  .only("Should not have any automatically detectable accessibility issues", () => {
-  let homePage: HomePage;
-  let productPage: ProductPage;
+test.describe.only(
+  "Should not have any automatically detectable accessibility issues",
+  { tag: "@accessibility" },
+  () => {
+    let homePage: HomePage;
+    let productPage: ProductPage;
 
-  // Before Hook
-  test.beforeEach(async ({ page }) => {
-    homePage = new HomePage(page);
-    productPage = new ProductPage(page);
+    // Before Hook
+    test.beforeEach(async ({ page }) => {
+      homePage = new HomePage(page);
+      productPage = new ProductPage(page);
 
-    await homePage.visitMainPage();
-  });
+      await homePage.visitMainPage();
+    });
 
-  test("Check Homepage accessibility issues", async ({ page }) => {
-    await homePage.visitMainPage();
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+    test("Check Homepage accessibility issues", async ({ page }) => {
+      await homePage.visitMainPage();
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
 
-  test("Check Category accessibility issues", async ({ page }) => {
-    await homePage.visitMainPage();
-    await homePage.openCategoryPage();
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+    test("Check Category accessibility issues", async ({ page }) => {
+      await homePage.visitMainPage();
+      await homePage.openCategoryPage();
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
 
-  test("Check Product Page accessibility issues", async ({ page }) => {
-    await homePage.visitMainPage();
-    await homePage.openCartPage();
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
-});
+    test("Check Product Page accessibility issues", async ({ page }) => {
+      await homePage.visitMainPage();
+      await homePage.openCartPage();
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  },
+);
