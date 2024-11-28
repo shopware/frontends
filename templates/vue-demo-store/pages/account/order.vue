aa2c1cb076165c46d08cd87ed44caf08881c8d32
mkucmus <maciejkucmus@gmail.com>
2024-11-28 10:47:36 +0100

    chore: cleanup

--- templates/vue-demo-store/pages/account/order.vue
+++ templates/vue-demo-store/pages/account/order.vue
@@ -85,7 +85,7 @@ await useAsyncData("getOrders", () => {
   return loadOrders({
     limit: limit.value,
     page: route.query.p ? Number(route.query.p) : defaultPage,
-    checkPromotions: true, // determines whether "paymentChangeable" list is available in the response
+    checkPromotion: true, // determines whether "paymentChangeable" list is available in the response
     associations: {
       stateMachineState: {},
     },
