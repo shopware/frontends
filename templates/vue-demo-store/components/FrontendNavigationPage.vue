646abd3a057b057c826b5e3a4d82496f795c254e
mkucmus <maciejkucmus@gmail.com>
2025-01-31 10:26:07 +0100

    chore: add cache key

--- templates/vue-demo-store/components/FrontendNavigationPage.vue
+++ templates/vue-demo-store/components/FrontendNavigationPage.vue
@@ -15,7 +15,7 @@ const { apiClient } = useShopwareContext();
 const errorDetails = ref();
 
 const { data, error } = await useAsyncData(
-  `cmsNavigation${props.navigationId}`,
+  `cmsNavigation${props.navigationId}-${route.query?.manufacturer || "empty"}`,
   async () => {
     console.warn(
       "useAsyncData",
