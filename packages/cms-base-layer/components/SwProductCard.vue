e603132a4cfb13d5642dd7d6e790968c61c4e2ed
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2025-02-13 13:21:37 +0100

    fix: product name tag

--- packages/cms-base-layer/components/SwProductCard.vue
+++ packages/cms-base-layer/components/SwProductCard.vue
@@ -235,11 +235,11 @@ const srcPath = computed(() => {
         :to="buildUrlPrefix(getProductRoute(product), getUrlPrefix())"
         data-testid="product-box-product-name-link"
       >
-        <h5
+        <div
           class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white min-h-60px"
         >
           {{ getProductName({ product }) }}
-        </h5>
+        </div>
       </RouterLink>
 
       <SwListingProductPrice
