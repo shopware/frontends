1c0860c98c88c5133b2e643d8a0fff1e24a47928
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2025-02-21 10:07:03 +0100

    feat: refactoring

--- packages/cms-base-layer/components/public/cms/element/CmsBlockHtml.vue
+++ packages/cms-base-layer/components/public/cms/element/CmsBlockHtml.vue
@@ -2,13 +2,23 @@
 import type { CmsBlockHtml } from "@shopware/composables";
 
 import { getCmsLayoutConfiguration } from "@shopware/helpers";
+import { h } from "vue";
 
 const props = defineProps<{
   content: CmsBlockHtml;
 }>();
 
 const { cssClasses, layoutStyles } = getCmsLayoutConfiguration(props.content);
+
+const HtmlComponent = () => {
+  return h("div", {
+    class: cssClasses,
+    style: layoutStyles,
+    innerHTML: props.content.slots[0].data.content || "",
+  });
+};
 </script>
+
 <template>
-   <div :class="cssClasses" v-html="props.content.slots[0].data.content || ''" :style="layoutStyles"  > </div>
+  <HtmlComponent />
 </template>
