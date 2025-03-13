8738529df594297cd7614b8c03d553fc68bb4f59
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2025-03-13 12:07:36 +0100

    feat: add bredcrumbs components

--- apps/storybook/nuxt.config.ts
+++ apps/storybook/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  extends: ["../../packages/layers/layout"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/storybook", "@unocss/nuxt"],
---
+++ apps/storybook/stories/layers/layout/components/LayoutBreadcrumbs.stories.ts
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import LayoutBreadcrumbs from "../../../../../../packages/layers/layout/components/LayoutBreadcrumbs.vue";
+import LayoutBreadcrumbsDivider from "../../../../../../packages/layers/layout/components/LayoutBreadcrumbsDivider.vue";
+import LayoutBreadcrumbsElement from "../../../../../../packages/layers/layout/components/LayoutBreadcrumbsElement.vue";
+
+const meta = {
+  title: "Layers/Layout/LayoutBreadcrumbs",
+  component: LayoutBreadcrumbs,
+  tags: ["autodocs"],
+} satisfies Meta<typeof LayoutBreadcrumbs>;
+
+export default meta;
+
+export const Primary: StoryObj<typeof LayoutBreadcrumbs> = {
+  render: (args) => ({
+    components: { LayoutBreadcrumbs },
+    setup() {
+      return { args };
+    },
+    template: '<LayoutBreadcrumbs v-bind="args" />',
+  }),
+  args: {
+    breadcrumbs: [
+      {
+        name: "Home",
+        path: "/",
+      },
+      {
+        name: "Page",
+        path: "/page",
+      },
+      {
+        name: "Current",
+        path: "/page/current",
+      },
+    ],
+  },
+};
+
+export const Divider: StoryObj<typeof LayoutBreadcrumbsDivider> = {
+  render: (args) => ({
+    components: { LayoutBreadcrumbsDivider },
+    setup() {
+      return { args };
+    },
+    template: "<LayoutBreadcrumbsDivider  />",
+  }),
+  args: {},
+};
+
+export const Element: StoryObj<typeof LayoutBreadcrumbsElement> = {
+  render: (args) => ({
+    components: { LayoutBreadcrumbsElement },
+    setup() {
+      return { args };
+    },
+    template: `<LayoutBreadcrumbsElement v-bind="args"  />`,
+  }),
+  args: {
+    label: "Label",
+  },
+};
