9e536e246c00c8ef68f860e147c8d5ea18096d83
Maciej D <7597086+mdanilowicz@users.noreply.github.com>
2025-04-22 10:28:20 +0200

    feat: enhance multi-sales-channel example (#1841)

--- examples/multi-sales-channel/pages/[...all].vue
+++ examples/multi-sales-channel/pages/[...all].vue
@@ -1,45 +1,65 @@
 <script setup lang="ts">
-import { getLanguageName, getTranslatedProperty } from "@shopware/helpers";
+const { sessionContext } = useSessionContext();
 
-const { languages, getLanguageCodeFromId } = useInternationalization();
-const { sessionContext, languageIdChain } = useSessionContext();
-
-const language = computed(() =>
-  languages.value.find((l) => languageIdChain.value === l.id),
-);
-
-const onChangeHandler = async (option: Event) => {
-  const id = (option.target as HTMLSelectElement).value;
-  const code = getLanguageCodeFromId(id);
-
-  navigateTo(`/${code}`, {
+const onChangeHandler = async (url: string) => {
+  navigateTo(`${url}`, {
     external: true,
   });
 };
+
+/**
+ * List of stores to be used in the application.
+ */
+const stores = [
+  {
+    name: "store1 - localhost:3000",
+    url: "/",
+  },
+  {
+    name: "store2 - localhost:3000/pl-PL",
+    url: "/pl-PL",
+  },
+];
 </script>
 
 <template>
-  <div>
+  <div class="container mx-auto p-4">
+    <h1>Multi sales channel example</h1>
+
+    <p>This example shows how to use the multi sales channel feature</p>
+
+    <h2>Assumptions:</h2>
+
+    <p>This example is using 2 sales channels:</p>
+
     <ul>
       <li>
-        Language: {{ language ? getLanguageName(language) : languageIdChain }}
+        Store 1 - default sales channel on
+        <span class="bg-red text-white px-1"> localhost:3000 </span>
       </li>
       <li>
-        Sales channel:
-        {{ getTranslatedProperty(sessionContext?.salesChannel, "name") }}
+        Store 2 - sales channel on
+        <span class="bg-red text-white px-1">localhost:3000/pl-PL</span>
       </li>
     </ul>
 
-    <select aria-label="Select language" @change="onChangeHandler">
-      <option
-        v-for="language in languages"
-        :key="language.id"
-        :value="language.id"
-        :selected="languageIdChain === language.id"
-        :label="getLanguageName(language)"
+    <hr />
+
+    <h3 class="text-center">
+      Selected storefront: {{ sessionContext?.salesChannel?.translated?.name }}
+    </h3>
+
+    <div class="grid grid-cols-1 gap-4 mt-6">
+      <div
+        v-for="store in stores"
+        @click="onChangeHandler(store.url)"
+        :key="store.name"
+        class="cursor-pointer p-4 border rounded-lg shadow-sm hover:shadow-md transition-shadow bg-white hover:bg-gray-100 text-center"
       >
-        {{ getLanguageName(language) }}
-      </option>
-    </select>
+        <span class="text-lg font-semibold text-gray-700">{{
+          store.name
+        }}</span>
+      </div>
+    </div>
   </div>
 </template>
