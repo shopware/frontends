326a947206ceda002cc4ab8752aac09932664cef
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2025-06-10 10:34:22 +0200

    feat: mobile search

--- templates/vue-starter-template/app/components/form/BaseInput.vue
+++ templates/vue-starter-template/app/components/form/BaseInput.vue
@@ -13,16 +13,16 @@ const props = defineSlots<{
 }>();
 </script>
 <template>
-
-
-<!-- <div class="self-stretch h-10 px-4 py-2 rounded-lg outline outline-1 outline-offset-[-1px] outline-[#cac4d0] inline-flex flex-col justify-start items-start gap-2.5">
-  <div data-show-dots="false" data-state="Placeholder" class="inline-flex justify-start items-center">
-    <div class="justify-start text-[#686470] text-sm font-normal font-['Inter'] leading-[21px]">Input text</div>
-  </div>
-</div> -->
-  <div class="flex items-center rounded-lg px-4 py-2 outline outline-1 outline-offset-[-1px] outline-outline-outline-variant text-surface-on-surface-variant">
+  <div
+    class="flex items-center rounded-lg px-4 py-2 outline outline-1 outline-offset-[-1px] outline-outline-outline-variant text-surface-on-surface-variant"
+  >
     <slot name="leftIcon" />
-    <input v-model="model" class="text-sm" type="text" :placeholder="placeholder"/>
+    <input
+      v-model="model"
+      class="text-sm"
+      type="text"
+      :placeholder="placeholder"
+    />
     <slot name="rightIcon" />
   </div>
 </template>
--- templates/vue-starter-template/app/components/layout/Header.vue
+++ templates/vue-starter-template/app/components/layout/Header.vue
@@ -2,22 +2,30 @@
 const { count: cartCount } = useCart();
 const { count: wishlistCount } = useWishlist();
 
+const mobileMenuActive = ref(true);
+
 const localePath = useLocalePath();
 const { formatLink } = useInternationalization(localePath);
 </script>
+
 <template>
   <div class="border-b">
     <div class="container mx-auto flex items-center justify-between py-3.5">
-      <NuxtLink :to="formatLink('/')">
-        <NuxtImg class="h-20 max-sm:h-10" src="/logo.svg"  alt="logo"/>
-      </NuxtLink>
-      <LayoutHeaderSearch class="max-sm:hidden" />
-      <div class="flex gap-4">
-        <LayoutHeaderSearchIcon class="hidden max-sm:block" />
-        <LayoutHeaderMyAccountIcon />
-        <LayoutHeaderWishlistIcon :counter="wishlistCount" />
-        <LayoutHeaderCartIcon :counter="cartCount" />
-      </div>
+      <template v-if="!mobileMenuActive">
+        <NuxtLink :to="formatLink('/')">
+          <NuxtImg class="h-20 max-sm:h-10" src="/logo.svg" alt="logo" />
+        </NuxtLink>
+        <LayoutHeaderSearch class="max-sm:hidden" />
+        <div class="flex gap-4">
+          <LayoutHeaderSearchIcon class="hidden max-sm:block" />
+          <LayoutHeaderMyAccountIcon />
+          <LayoutHeaderWishlistIcon :counter="wishlistCount" />
+          <LayoutHeaderCartIcon :counter="cartCount" /></div
+      ></template>
+      <template v-else>
+        <LayoutHeaderSearch  />
+        <BaseButton label="Close" />
+      </template>
     </div>
   </div>
 </template>
