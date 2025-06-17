4475fa785b313baf2cdf9fe110d706ea8fbccace
patzick <13100280+patzick@users.noreply.github.com>
2025-06-17 12:20:51 +0200

    fix: price formatting independent of env

--- packages/composables/src/usePrice/usePrice.ts
+++ packages/composables/src/usePrice/usePrice.ts
@@ -76,6 +76,7 @@ function _usePrice(params?: {
     return new Intl.NumberFormat(currencyLocale.value, {
       style: "currency",
       currency: currencyCode.value,
+      currencyDisplay: "symbol",
     }).format(+value);
   }
 
