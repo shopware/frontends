cdf9f47a42c1d91266527e6e732a00ed3acd1954
mkucmus <maciejkucmus@gmail.com>
2025-07-18 14:07:44 +0200

    feat: uno config update

--- packages/cms-base-layer/components/public/cms/element/CmsElementProductName.vue
+++ packages/cms-base-layer/components/public/cms/element/CmsElementProductName.vue
@@ -6,5 +6,6 @@ defineProps<{
 }>();
 </script>
 <template>
-  <CmsElementText :content="content as any" />
+  <!-- there is no css config coming from API for this element so we don't need to merge -->
+  <CmsElementText :content="content as any" class="self-stretch justify-start text-surface-on-surface text-4xl font-normal font-['Noto_Serif'] leading-[60px]" />
 </template>
