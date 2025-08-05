a344abba579c91c4f775e7be27ed882ca420fdc2
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2025-08-05 10:37:39 +0200

    fix(api-gen): allow shared paremeters to be available in schema resolver (#1942)
    * fix(api-gen): allow shared paremeters to be available in schema resolver
    
    * fix(api-gen): allow shared paremeters to be available in schema resolver
    
    * chore: fix ts error

---
+++ .changeset/warm-maps-help.md
@@ -0,0 +1,5 @@
+---
+"@shopware/api-gen": patch
+---
+
+Allow shared parameters to be available in schema resolver.
---
+++ packages/api-gen/tests/snapshots-transformOpenApiTypes/requestParamsReference.example.ts
@@ -0,0 +1,42 @@
+export type paths = {
+  "/customer/wishlist/{productId}": {
+    post: operations["myRequestWithParams"];
+  };
+};
+
+export type webhooks = Record<string, never>;
+export type components = {
+  schemas: {};
+  parameters: {
+    /** Accepted response content types */
+    accept: string;
+    /** Content type of the request */
+    contentType: string;
+    criteriaIds: string[];
+  };
+};
+
+export type $defs = Record<string, never>;
+
+export type external = Record<string, never>;
+
+export type operations = {
+  myRequestWithParams: {
+    parameters: {
+      query: {
+        /** A list of product identifiers. */
+        ids?: components["parameters"]["criteriaIds"];
+      };
+    };
+    responses: {
+      /** The updated cart. */
+      200: {
+        content: {
+          "application/json": {
+            success: boolean;
+          };
+        };
+      };
+    };
+  };
+};
