01a7ef8c33e70594397715cd334293c4aadf4236
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2025-08-11 12:57:11 +0200

    feat: add breadcrumbs layout component

---
+++ templates/vue-starter-template/app/components/layout/breadcrumbs/Element.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps<{
+  link?: string;
+}>();
+
+defineSlots<{
+  default(): unknown;
+}>();
+</script>
+<template>
+  <component :is="link ? NuxtLink : 'div'" v-bind="link ? { to: link } : {}">
+    <span
+    :class="{link: `hover:underline hover:decoration-brand-primary-hover hover:decoration-2 hover:underline-offset-4 hover:text-brand-primary-hover`}"
+      class="justify-start leading-normal no-underline text-surface-on-surface"
+    >
+      <slot />
+    </span>
+  </component>
+</template>
