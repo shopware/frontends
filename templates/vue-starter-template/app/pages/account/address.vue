1644b6671a15f3e8c9c36b3098ec812046f743e2
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2025-09-15 21:38:36 +0200

    feat(account): add address management page

---
+++ templates/vue-starter-template/app/pages/account/address.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const { user } = useUser();
+function handleAddAddress() {
+  console.log("add address");
+}
+</script>
+
+<template>
+  <NuxtLayout name="account">
+    <div>
+      <AccountPageHeader
+        class="mb-14"
+        :title="$t('account.address.header')"
+        :subtitle="$t('account.address.subHeader')"
+      />
+
+      <FormBaseButton
+        class="mb-14"
+        variant="secondary"
+        @click="handleAddAddress"
+      >
+        + {{ $t("account.address.addAddressButton") }}</FormBaseButton
+      >
+
+      <div class="block md:flex gap-10 mb-10">
+        <div class="flex-1 mb-10">
+          <AccountSectionHeader
+            class="mb-4"
+            :title="$t('account.address.defaultBillingAddressSectionHeader')"
+          />
+          <AccountAddressDataSection
+            v-if="user?.defaultBillingAddress"
+            :address="user.defaultBillingAddress"
+          />
+        </div>
+
+        <div class="flex-1 mb-10">
+          <AccountSectionHeader
+            class="mb-4"
+            :title="$t('account.address.defaultShippingAddressSectionHeader')"
+          />
+          <AccountAddressDataSection
+            v-if="user?.defaultShippingAddress"
+            :address="user.defaultShippingAddress"
+          />
+        </div>
+      </div>
+
+      <div class="mb-10">
+        <AccountSectionHeader
+          class="mb-4"
+          :title="$t('account.address.availableAddressesSectionHeader')"
+        />
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
