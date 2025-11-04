c98d9c0b3c25736138c30e007727985474e01e00
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2025-11-04 12:38:25 +0100

    feat: improve wishlist page functionality with pagination and conditional data fetching

--- templates/vue-starter-template/app/pages/wishlist.vue
+++ templates/vue-starter-template/app/pages/wishlist.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+const route = useRoute();
+const router = useRouter();
+
+const defaultLimit = 15;
+const defaultPage = 1;
+
 const loading = ref(false);
 
 const {
@@ -9,13 +15,56 @@ const {
   limit,
 } = useWishlist();
 
+const initialPage = route.query.p ? Number(route.query.p) : defaultPage;
+const initialLimit = route.query.limit
+  ? Number(route.query.limit)
+  : defaultLimit;
+
 async function handleChangePage(page: number): Promise<void> {
-  await getWishlistProducts({ page, limit: limit.value });
+  await router.push({
+    query: {
+      ...route.query,
+      p: page,
+      limit: limit.value,
+    },
+  });
+  getWishlistProductsProxy();
 }
 
 async function handleChangeSize(size: number): Promise<void> {
-  await getWishlistProducts({ page: currentPage.value, limit: size });
+  await router.push({
+    query: {
+      ...route.query,
+      p: defaultPage,
+      limit: size,
+    },
+  });
+  await getWishlistProductsProxy();
 }
+
+async function getWishlistProductsProxy() {
+  loading.value = true;
+  try {
+    await getWishlistProducts({
+      page: initialPage,
+      limit: initialLimit,
+    });
+  } catch (error) {
+    console.error(error);
+    // TODO: handle error
+  } finally {
+    loading.value = false;
+  }
+}
+
+onMounted(async () => {
+  loading.value = true;
+  await getWishlistProducts({
+    page: initialPage,
+    limit: initialLimit,
+  });
+  loading.value = false;
+});
 </script>
 
 <template>
@@ -30,7 +79,7 @@ async function handleChangeSize(size: number): Promise<void> {
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-10"
     >
       <template v-if="loading">
-        <WishlistProductTileSkeleton v-for="n in 8" :key="n" />
+        <WishlistProductTileSkeleton v-for="n in limit || 15" :key="n" />
       </template>
       <template v-else>
         <WishlistProductTile
