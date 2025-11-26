a42e6b824b1ced3f50e58a53d40b91f9e1f41ba3
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2025-11-26 09:40:00 +0100

    feat: changes after CR

--- templates/vue-starter-template/app/components/layout/MiniCart.vue
+++ templates/vue-starter-template/app/components/layout/MiniCart.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onClickOutside } from "@vueuse/core";
 
-const { t } = useI18n();
 const localePath = useLocalePath();
 const { cartItems, subtotal, removeItemById } = useCart();
 
@@ -37,7 +36,7 @@ onClickOutside(miniCartContainer, () => {
       <div
         class="text-surface-on-surface text-2xl font-normal font-['Noto_Serif'] leading-9"
       >
-        {{ t("cart.miniCart.title") }}
+        {{ $t("cart.miniCart.title") }}
       </div>
       <FormIconButton type="ghost" @click.stop="handleCloseMiniCart">
         <Icon name="shopware:times-s" class="w-3 h-3" />
@@ -57,7 +56,7 @@ onClickOutside(miniCartContainer, () => {
     <div class="px-6 py-3 border border-t-0 bg-surface-surface-container-low">
       <div class="flex items-center justify-between mb-2">
         <div class="text-surface-on-surface">
-          {{ t("cart.miniCart.subtotal") }}
+          {{ $t("cart.miniCart.subtotal") }}
         </div>
         <SharedPrice
           class="text-surface-on-surface font-bold leading-6"
@@ -65,7 +64,7 @@ onClickOutside(miniCartContainer, () => {
         />
       </div>
       <div class="text-right text-surface-on-surface-variant leading-6 mb-6">
-        {{ t("cart.miniCart.taxEstimation") }}
+        {{ $t("cart.miniCart.taxEstimation") }}
       </div>
       <NuxtLink
         :to="localePath('/checkout')"
