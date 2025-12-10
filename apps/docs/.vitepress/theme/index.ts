792cac8abcb9c51944661eee9e6cf6349d2432cc
mkucmus <maciejkucmus@gmail.com>
2025-12-10 10:49:05 +0100

    chore: cleanup

--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -1,9 +1,4 @@
 import { SWAGTheme } from "vitepress-shopware-docs";
-// import './styles/index.css'
-import type { App } from "vue";
-// Ai component
-// import AI from "./components/AI.vue";
-import PageRef from "./components/PageRef.vue";
 import "./custom.css";
 
 export default SWAGTheme();
