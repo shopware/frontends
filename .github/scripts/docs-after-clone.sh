201376b15e1204178773f8589d851a0e7cae50cc
Bojan Rajh <b.rajh@shopware.com>
2025-12-11 14:08:02 +0100

    fix: jq

--- .github/scripts/docs-after-clone.sh
+++ .github/scripts/docs-after-clone.sh
@@ -7,7 +7,8 @@ echo "Removing postinstall from ./package.json"
 sed -i '/^    "postinstall"/d' "$1"/package.json
 
 echo "Removing prepare from ./examples/amazon-pay-button-example/package.json"
-sed -i '/^    "prepare"/d' "$1"/examples/amazon-pay-button-example/package.json
+jq 'del(.scripts.prepare)' "$1"/examples/amazon-pay-button-example/package.json > "$1"/examples/amazon-pay-button-example/package.json.tmp \
+&& mv "$1"/examples/amazon-pay-button-example/package.json.tmp "$1"/examples/amazon-pay-button-example/package.json
 
 echo "Installing root pnpm"
 pnpm -C "$1" i
