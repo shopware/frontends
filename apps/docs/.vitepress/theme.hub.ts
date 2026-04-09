b704f564d33bed464f59ad48259391f9da749260
Bojan Rajh <b.rajh@shopware.com>
2026-04-09 11:41:59 +0200

    fix(docs): design tokens

--- apps/docs/.vitepress/theme.hub.ts
+++ apps/docs/.vitepress/theme.hub.ts
@@ -1,5 +1,7 @@
 import ComposablesList from "./theme/components/ComposablesList.vue";
+import ColorDesignTokens from "./theme/components/ColorDesignTokens.vue";
 
 export default ({ app }) => {
   app.component("ComposablesList", ComposablesList);
+  app.component("ColorDesignTokens", ColorDesignTokens);
 };
--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -1,9 +1,7 @@
 import { SWAGTheme } from "vitepress-shopware-docs";
-import ColorDesignTokens from "./components/ColorDesignTokens.vue";
 import "./custom.css";
 
 export default SWAGTheme({
   enhanceApp({ app }) {
-    app.component("ColorDesignTokens", ColorDesignTokens);
   },
 });
