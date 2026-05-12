02eca5a16d01c3c8e47e4280a46a94bc5daa93c3
Maciej D <7597086+mdanilowicz@users.noreply.github.com>
2026-05-12 14:38:35 +0200

    feat(vue-starter-template): add ShopwareBanner component to enhance user guidance (#2441)
    * feat: add ShopwareBanner component to enhance user guidance
    
    * feat: format
    
    * feat: changes after CR

--- templates/vue-starter-template/app/layouts/default.vue
+++ templates/vue-starter-template/app/layouts/default.vue
@@ -47,6 +47,10 @@ const { data: footerData } = useAsyncData("mainFooterNavigation", () => {
 provide("swNavigation-footer-navigation", footerData);
 </script>
 <template>
+  <DevOnly>
+    <ShopwareBanner />
+  </DevOnly>
+
   <div class="flex flex-col min-h-screen">
     <header>
       <LayoutMetaNavigation
