38b4e62f1c529480a12ac300783c3e12ac9f1dda
mkucmus <maciejkucmus@gmail.com>
2026-05-27 11:34:19 +0200

    fix(templates): avoid type cast and add no-cms fallback in FrontendLandingPage

--- templates/vue-starter-template/app/components/FrontendLandingPage.vue
+++ templates/vue-starter-template/app/components/FrontendLandingPage.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { getCmsBreadcrumbs } from "@shopware/helpers";
 import { useLandingSearch } from "#imports";
-import type { Schemas } from "#shopware";
 
 const props = defineProps<{
   navigationId: string;
@@ -11,20 +10,10 @@ const { search } = useLandingSearch();
 
 const { data: landingResponse, error } = await useAsyncData(
   `cmsLanding${props.navigationId}`,
-  async () => {
-    const landingPage = await search(props.navigationId, {
-      withCmsAssociations: true,
-    });
-    return landingPage;
-  },
+  () => search(props.navigationId, { withCmsAssociations: true }),
 );
 
-if (landingResponse.value) {
-  const breadcrumbs = getCmsBreadcrumbs(landingResponse.value);
-  useBreadcrumbs(breadcrumbs);
-}
-
-if (!landingResponse?.value) {
+if (!landingResponse.value) {
   console.error("[FrontendLandingPage.vue]", error.value?.message);
   throw createError({
     statusCode: 500,
@@ -32,11 +21,24 @@ if (!landingResponse?.value) {
   });
 }
 
-const landingPage = landingResponse as Ref<Schemas["LandingPage"]>;
+useBreadcrumbs(getCmsBreadcrumbs(landingResponse.value));
+
+const landingPage = computed(() => {
+  if (!landingResponse.value) {
+    throw createError({
+      statusCode: 500,
+      message: error.value?.message,
+    });
+  }
+  return landingResponse.value;
+});
 useCmsHead(landingPage, { mainShopTitle: "Shopware Frontends Demo Store" });
 </script>
 
 <template>
   <LayoutBreadcrumbs />
   <CmsPage v-if="landingResponse?.cmsPage" :content="landingResponse.cmsPage" />
+  <div v-else class="container mx-auto bg-white flex flex-col">
+    <span>😱 cmsPage is missing.</span>
+  </div>
 </template>
