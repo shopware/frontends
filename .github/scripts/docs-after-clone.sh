c3cbd41e5842a02c11652753fc90705a83634bc2
Bojan Rajh <b.rajh@shopware.com>
2026-06-03 16:34:11 +0200

    fix: remove workaround (not needed anymore?)

--- .github/scripts/docs-after-clone.sh
+++ .github/scripts/docs-after-clone.sh
@@ -3,12 +3,5 @@ set -e
 
 [[ -z "$1" ]] && echo "Missing working directory argument" && exit 1
 
-echo "Removing postinstall from ./package.json"
-sed -i '/^    "postinstall"/d' "$1"/package.json
-
-echo "Removing prepare from ./examples/amazon-pay-button-example/package.json"
-jq 'del(.scripts.prepare)' "$1"/examples/amazon-pay-button-example/package.json > "$1"/examples/amazon-pay-button-example/package.json.tmp \
-&& mv "$1"/examples/amazon-pay-button-example/package.json.tmp "$1"/examples/amazon-pay-button-example/package.json
-
 echo "Installing root pnpm"
 pnpm -C "$1" i
