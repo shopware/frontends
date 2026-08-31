1474c20ac21988463977a8a6d6fb2ce193089d11
Maciej <7597086+mdanilowicz@users.noreply.github.com>
2026-08-31 14:29:16 +0200

    refactor: simplify type mapping in RenameByT for better readability

--- packages/api-client/src/createAPIClient.ts
+++ packages/api-client/src/createAPIClient.ts
@@ -22,11 +22,9 @@ type SimpleUnionPick<T, K extends keyof T> = T extends unknown
   : never;
 
 type RenameByT<T, U> = {
-  [K in keyof U as K extends keyof T
-    ? T[K] extends string
-      ? T[K]
-      : never
-    : K]: K extends keyof U ? U[K] : never;
+  [
+    K in keyof U as K extends keyof T ? (T[K] extends string ? T[K] : never) : K
+  ]: K extends keyof U ? U[K] : never;
 };
 
 export type RequestReturnType<
